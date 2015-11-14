608ed6f8fa4ed0802c87a97a2b65d5d1499c5662
ChienHsin-Chan <hcchien@ChienHsin-ChandeMacBook-Pro.local>
2015-11-14 10:49:07 +0800

    update the service port to 8080

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ import crateRoutes from 'routes/index';
 import { Provider } from 'react-redux';
 
 let server = new Express();
-let port = 3000;
+let port = 8080;
 
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'ejs');
