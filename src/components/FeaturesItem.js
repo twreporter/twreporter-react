8cf8e92e5ebded178337764c6b0ff52ef2b42f82
nickhsine <nickhsine97753017@gmail.com>
2015-11-27 19:15:04 +0800

    use variable

--- src/components/FeaturesItem.js
+++ src/components/FeaturesItem.js
@@ -14,13 +14,13 @@ export default class FeaturesItem extends Component {
 
     render() {
         const { article } = this.props
-        article.firstImage = "https://twreporter.atavist.com/data/files/organization/60826/image/derivative/scale~1185x0~" + article.firstImage
+        let firstImage = "https://twreporter.atavist.com/data/files/organization/60826/image/derivative/scale~1185x0~" + article.firstImage
         var style = { backgroundImage: 'url(' + article.firstImage + ')' }
         return (
             <li className="listing-item" key={article.id}>
                 <a href={article.url}>
                     <Parallex>
-                        <img className="listing-img" src={article.firstImage}/>
+                        <img className="listing-img" src={firstImage}/>
                     </Parallex>
                     <div className="listing-projectborder">
                         <div className="listing-project">
