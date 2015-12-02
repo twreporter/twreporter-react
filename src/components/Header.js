0b7e57b46fb5d6947530a2dcfb2a09eeb37274aa
ChienHsin-Chan <hcchien@ChienHsin-ChandeMacBook-Pro.local>
2015-12-02 09:07:31 +0800

    test the social share button and fixed some category layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React, { Component, PropTypes } from 'react'
+import { FacebookLikeButton, FacebookShareButton, TwitterButton } from 'react-social-buttons'
 
 export default class Header extends Component {
     constructor(props) {
         super(props)
     }
     render() {
+        let url = "https://www.twreporter.org/";
         return (
-            <div>
+            <div className="uh">
+                <FacebookShareButton url={url} />
                 This is header
             </div>
         )
