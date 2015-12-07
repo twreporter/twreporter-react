35b78030276d51062f3b5e54d7f27412c468b15e
nickhsine <nickhsine97753017@gmail.com>
2015-12-07 16:40:11 +0800

    handle browser resizing

--- src/components/FeaturesItem.js
+++ src/components/FeaturesItem.js
@@ -1,40 +1,61 @@
 import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
 
 if (process.env.BROWSER) {
     require("./FeaturesItem.css");
 }
+
 export default class FeaturesItem extends Component {
     constructor(props, context) {
         super(props, context)
-    }
-    handleResize() {
-        var listingItem = document.getElementsByClassName('listing-item')
-        for (var i = 0; i < listingItem.length; i++) {
-            listingItem[i].style.height = window.screen.availWidth*0.75 + 'px'
-        }
+
+        // scrollController and scrollScene are used for ScrollMagic
+        this.scrollController = null
+        this.scrollScene = null
     }
 
-    componentDidMount() {
-        //this.handleResize()
-        //window.addEventListener('resize', this.handleResize);
-
+    _handleResize() {
         // only for desktop, we have the parallax animation
         if (window.innerWidth  > 800 ) {
-            // init controller
-            let controller = new ScrollMagic.Controller();
+
+            if (!this.scrollController) {
+                // init controller
+                this.scrollController = new ScrollMagic.Controller();
+            } else {
+                // destroy controller and scene
+                this.scrollScene.destroy(true)
+                this.scrollController.destroy(true)
+            }
 
             // create a scene
-            let scene = new ScrollMagic.Scene({
+            this.scrollScene = new ScrollMagic.Scene({
                 triggerElement: '#parallax-trigger'+this.props.article.id,
-                triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport,
+                triggerHook: 0, // don't trigger until #parallax-trigger hits the top of the viewport,
                 // right now we set is as 90% by heuristic.
                 // TODO compute the best duration percentage
                 duration: '90%'
             })
             .setPin('#parallax-trigger'+this.props.article.id, {pushFollowers: false}) // pins the element for the the scene's duration
             .addIndicators()
-            .addTo(controller)
+            .addTo(this.scrollController)
+
+        } else {
+            // destroy controller if existed
+            if (this.scrollController) {
+                this.scrollController.destroy(true)
+                this.scrollController = null
+            }
+            // destroy scene if existed
+            if (this.scrollScene) {
+                this.scrollScene.destroy(true)
+                this.scrollScene = null
+            }
         }
+    }
+
+    componentDidMount() {
+        this._handleResize()
+        window.addEventListener('resize', this._handleResize.bind(this));
     }
 
     render() {
@@ -44,11 +65,12 @@ export default class FeaturesItem extends Component {
         var d = new Date()
         d.setTime(article.lastPublish*1000)
         var d_str = d.toISOString().substring(0,10);
+
         return (
             <li className="listing-item" key={article.id}>
                 <a href={url}>
-                    <div ref='parallaxImg' id={'parallax-trigger'+this.props.article.id} className="featuresimage-wrap">
-                        <img className="listing-img" src={firstImage}/>
+                    <div id={'parallax-trigger'+this.props.article.id} className="featuresimage-wrap">
+                        <img  ref='listImg' width='1800px' height='1200px' className="listing-img" src={firstImage}/>
                     </div>
                     <div ref='parallaxIndicator'>
                         <div className="feature-categorycontainer">
