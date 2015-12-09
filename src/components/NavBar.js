6687702709e2517044ffcbeefa60b2074bda5795
ChienHsin-Chan <hcchien@gmail.com>
2015-12-09 18:53:51 +0800

    update the Navbar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ var Menu = require('react-burger-menu').slide;
 
 if (process.env.BROWSER) {
     require("./NavBar.css");
+    require("./NavBurg.css");
 }
 
 export default class NaviBar extends Component {
@@ -25,6 +26,17 @@ export default class NaviBar extends Component {
                         <li className="nav-item"><a id="contact" className="menu-item" href="/contact">專題</a></li>
                         <li className="nav-item"><a id="contact" className="menu-item" href="/contact">媒體</a></li>
                     </ul>
+                </div>
+                <div className="berg">
+                    <Menu right>
+                        <a id="home" className="menu-item" href="/">台灣</a>
+                        <a id="about" className="menu-item" href="/about">國際</a>
+                        <a id="contact" className="menu-item" href="/contact">評論</a>
+                        <a id="contact" className="menu-item" href="/contact">文化</a>
+                        <a id="contact" className="menu-item" href="/contact">影像</a>
+                        <a id="contact" className="menu-item" href="/contact">專題</a>
+                        <a id="contact" className="menu-item" href="/contact">媒體</a>
+                    </Menu>
                 </div>
             </div>
         )
