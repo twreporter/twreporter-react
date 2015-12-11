eafb42991771123a9ed4ca3932e8d0443585e516
nickhsine <nickhsine97753017@gmail.com>
2015-12-11 16:54:36 +0800

    add lib/date-transformer

---
+++ src/lib/date-transformer.js
@@ -0,0 +1,7 @@
+export const ts2yyyymmdd = (ts, separator) => {
+    let date = new Date(ts);
+    let year = date.getFullYear();
+    let mon = date.getMonth() + 1;
+    let day = date.getDate();
+    return [year, mon, day].join(separator);
+};
