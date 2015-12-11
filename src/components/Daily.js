23635c3fd6a24f20d26f9de197ed938a0724a4df
nickhsine <nickhsine97753017@gmail.com>
2015-12-11 16:42:46 +0800

    fix indent

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -24,27 +24,27 @@ export default class Daily extends Component {
 		<div className="daily-itemlistwrapprt">
 		<ul className="daily-itemlist">
 		    { _.map(dailyTop, (a, index) => {
-
-            const pubDate = ts2yyyymmdd(a.lastPublish * 1000, '.');
-            if (a.isPublishedVersion) {
-			    let thumbnail;
-                if (a.firstImage && (a.firstImage.indexOf('.png') > -1 || a.firstImage.indexOf('.jpg') > -1)) {
-                    thumbnail = a.firstImage;
-                } else if (a.previewImage) {
-                   thumbnail = a.previewImage
-                } else if (a.facebookImage) {
-                    thumbnail = "https://twreporter.atavist.com/data/files/organization/60826/image/derivative/scale~1200x1200~" + a.facebookImage
+                const pubDate = ts2yyyymmdd(a.lastPublish * 1000, '.');
+                if (a.isPublishedVersion) {
+                    let thumbnail;
+                    if (a.firstImage && (a.firstImage.indexOf('.png') > -1 || a.firstImage.indexOf('.jpg') > -1)) {
+                        thumbnail = a.firstImage;
+                    } else if (a.previewImage) {
+                        thumbnail = a.previewImage
+                    } else if (a.facebookImage) {
+                        thumbnail = "https://twreporter.atavist.com/data/files/organization/60826/image/derivative/scale~1200x1200~" + a.facebookImage
+                    }
+                    let url = (a.storyLink) ? a.storyLink : "https://www.twreporter.org/a/" + a.slug
+                    return (
+                        <a href={url} key={a.id}>
+                            <li className="daily-item">
+                                <img className="daily-image" src={thumbnail}/>
+                                <div className="daily_lastpublish">{pubDate}</div>
+                                <div className="daily-title">{a.title}</div>
+                            </li>
+                        </a>
+                    );
                 }
-			    let url = (a.storyLink) ? a.storyLink : "https://www.twreporter.org/a/" + a.slug
-			    return (
-				<a href={url} key={a.id}>
-				    <li className="daily-item">
-					    <img className="daily-image" src={thumbnail}/>
-                        <div className="daily_lastpublish">{pubDate}</div>
-				        <div className="daily-title">{a.title}</div>
-				    </li>
-				</a>
-			    );}
 			})
 		    }
 		</ul>
