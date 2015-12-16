2f983544eeaf65bb2c583f5497e83a6c3d45a8dd
nickhsine <nickhsine97753017@gmail.com>
2015-12-16 22:38:00 +0800

    Revert "add schema"
    This reverts commit 83e8dd1e6d25adcf332e3e9e07cba34787f39a4c.

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,28 +1,9 @@
-import { Schema, arrayOf, normalize } from 'normalizr';
 import { camelizeKeys } from 'humps';
 import superAgent from 'superagent';
 import Promise from 'bluebird';
 import _ from 'lodash';
 
 export const CALL_API = Symbol('CALL_API');
-
-// We use this Normalizr schemas to transform API responses from a nested form
-// to a flat form where repos and users are placed in `entities`, and nested
-// JSON objects are replaced with their IDs. This is very convenient for
-// consumption by reducers, because we can easily build a normalized tree
-// and keep it updated as we fetch more data.
-
-// Read more about Normalizr: https://github.com/gaearon/normalizr
-
-const articleSchema = new Schema('articles', {
-    idAttribute: 'id'
-})
-
-// Schemas for Github API responses.
-export const Schemas = {
-    ARTICLE: articleSchema,
-    ARTICLES : arrayOf(articleSchema)
-}
 
 export default store => next => action => {
     if ( ! action[CALL_API] ) {
@@ -32,9 +13,9 @@ export default store => next => action => {
     let { getState } = store;
     let deferred = Promise.defer();
     // handle 401 and auth here
-    const { method, url, types, params, schema } = request;
+    let { method, url, types, params } = request;
     const [ requestType, successType, failureType ] = types;
-    superAgent[method](url).timeout(500).send(params)
+    superAgent[method](url).timeout(200).send(params)
         .end((err, res)=> {
             if ( res && res.text ) {
                 if (res.ok) {
