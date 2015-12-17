a40b4e9029bd3c4c140e6bc9ee0cf1c2b004f2ce
nickhsine <nickhsine97753017@gmail.com>
2015-12-17 13:52:49 +0800

    detect gif as image

--- src/lib/image-composer.js
+++ src/lib/image-composer.js
@@ -4,6 +4,14 @@ const mobileScale = 'scale~1200x0~';
 const desktopImagePrefix = imagePrefix + desktopScale;
 const mobileImagePrefix = imagePrefix + mobileScale;
 
+const isImage = (image) => {
+  if (image && (image.indexOf('.jpg') > -1 || image.indexOf('.png') > -1 || image.indexOf('.gif') > -1)) {
+    return true;
+  } else {
+    return false
+  }
+}
+
 export const imageComposer = (article, device) => {
     let image;
     let firstImage = article.firstImage;
@@ -14,14 +22,14 @@ export const imageComposer = (article, device) => {
         return device === 'desktop' ? desktopImagePrefix + facebookImage : mobileImagePrefix + facebookImage;
     }
 
-    if (firstImage && (firstImage.indexOf('.jpg') > -1 || firstImage.indexOf('.png') > -1)) {
+    if (isImage(firstImage)) {
         image = firstImage;
-    } else {
+    } else if (isImage(previewImage)) {
         image = previewImage;
     }
 
     let source;
-    if (image && (image.indexOf('.jpg') > -1 || image.indexOf('.png') > -1)) {
+    if (image) {
         const regex = /.+~(.+)/;
         source = image.match(regex);
         source = source ? source[1] : '';
