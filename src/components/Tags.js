cf5e99872f193258a8a204cba0cc82bf3580cb7b
ChienHsin-Chan <hcchien@gmail.com>
2015-12-20 00:47:39 +0800

    fixed the category page

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import Category from './Category';
 import _ from 'lodash';
+import { imageComposer } from '../lib/image-composer.js'
 if (process.env.BROWSER) {
     require("./Tags.css")
 }
@@ -12,29 +13,29 @@ export default class Tags extends Component {
     }
 
     render() {
-        const { articles } = this.props
+        const { articles, device } = this.props
         let cat_display = "台灣"
-        if (articles && articles.length > 0) {
+        let list = articles.items
+        if (list && list.length > 0) {
         return (
             <div className="category-items">
                 <div className="category-name">
                     <Category>{cat_display}</Category>
                 </div>
                 <ul className="tag-listing">
-                { _.map(articles, (a) => {
+                { _.map(list, (a) => {
+                    const image = imageComposer(a, device) 
                     var d = new Date()
                     d.setTime(a.lastPublish*1000)
                     var d_str = d.toISOString().substring(0,10)
                     var re = /^[\w\d]/
-                    let img_existing = re.exec(a.firstImage)
                     var url = "https://www.twreporter.org/a/" + a.slug
-                    if (img_existing != null) {
-                        let thumbnail = "https://www.twreporter.org/data/files/organization/60826/image/derivative/scale~600x600~" + a.firstImage
+                    if (image) {
                         return (
                             <li className="tag-item" key={a.id}>
                                 <a href={url}>
                                     <div className="itemimage-wrap">
-                                        <img className="category-itemimage" src={thumbnail}/>
+                                        <img className="category-itemimage" src={image}/>
                                     </div>
                                     <div className="tag-itemdesc">
                                         <div className="tag-itemtitle">{a.title}</div>
