90267ee4a0b62850a8e2d70392511e673cfc7bc9
nickhsine <nickhsine97753017@gmail.com>
2015-12-22 17:16:24 +0800

    make nav burger work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,41 +9,65 @@ if (process.env.BROWSER) {
   require('./NavBurg.css')
 }
 
+class DesktopNav extends Component {
+  render() {
+    const { children } = this.props
+    let _children = []
+    children.map((child, i) => {
+      _children.push(
+        <li className="nav-item" key={i}>
+          {child}
+        </li>
+      )
+    })
+    return (
+      <div className="listing-menu">
+        <ul className="menu-items">
+          {_children}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class MobileNav extends Component {
+  render() {
+    return (
+      <div className="berg">
+        <Menu right>
+          {this.props.children}
+        </Menu>
+      </div>
+    )
+  }
+}
+
 export default class NaviBar extends Component {
   constructor(props) {
     super(props)
   }
   render() {
+    const { device } = this.context
+    let Nav = device === 'desktop' ? DesktopNav : MobileNav
     return (
       <div className="nav-menu">
         <div className="nav_logo">
           <a href="#"><img src="/asset/logo.png" height="58px" /></a>
-          </div>
-          <div className="listing-menu">
-            <ul className="menu-items">
-              <li className="nav-item">
-                <Link className="menu-item" to="/category/cat-台灣">台灣</Link>
-              </li>
-              <li className="nav-item"><a id="about" className="menu-item" href="/about">國際</a></li>
-              <li className="nav-item"><a id="contact" className="menu-item" href="/contact">評論</a></li>
-              <li className="nav-item"><a id="contact" className="menu-item" href="/contact">文化</a></li>
-              <li className="nav-item"><a id="contact" className="menu-item" href="/contact">影像</a></li>
-              <li className="nav-item"><a id="contact" className="menu-item" href="/contact">專題</a></li>
-              <li className="nav-item"><a id="contact" className="menu-item" href="/contact">媒體</a></li>
-            </ul>
-          </div>
-          <div className="berg">
-            <Menu right>
-              <a id="home" className="menu-item" href="/">台灣</a>
-              <a id="about" className="menu-item" href="/about">國際</a>
-              <a id="contact" className="menu-item" href="/contact">評論</a>
-              <a id="contact" className="menu-item" href="/contact">文化</a>
-              <a id="contact" className="menu-item" href="/contact">影像</a>
-              <a id="contact" className="menu-item" href="/contact">專題</a>
-              <a id="contact" className="menu-item" href="/contact">媒體</a>
-            </Menu>
-          </div>
+        </div>
+        <Nav>
+          <Link className="menu-item" to="/category/cat:台灣">台灣</Link>
+          <Link className="menu-item" to="/category/cat:國際">國際</Link>
+          <Link className="menu-item" to="/category/review">評論</Link>
+          <Link className="menu-item" to="/category/cat:文化">文化</Link>
+          <Link className="menu-item" to="/category/cat:影像">影像</Link>
+          <Link className="menu-item" to="/category/hp-projects">專題</Link>
+          <Link className="menu-item" to="/category/cat:媒體">媒體</Link>
+        </Nav>
       </div>
     )
   }
+}
+
+NaviBar.contextTypes = {
+  device: React.PropTypes.string
 }
