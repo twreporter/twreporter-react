f2e381f2c57395650692a685d6754c0fa1f92d7c
garfieldduck <garfieldduck@gmail.com>
2016-01-15 00:14:38 +0800

    add hamburger menu bar in mobile device

--- src/components/MobileNavBar.js
+++ src/components/MobileNavBar.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import Sticky from 'react-sticky'
+import { categoryPath } from '../lib/constants'
 // import { categoryPath } from '../lib/constants'
 
 // require('react-burger-menu').slide
 
 if (process.env.BROWSER) {
   require('./NavBar.css')
-  require('./NavBurg.css')
+  require('./MobileNaviBar.css')
 }
 
 export default class MobileNaviBar extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      open: false
+    }
   }
+
   render() {
-    // const { taiwanPath, reviewPath, photographyPath, intlPath } = categoryPath
+    const { taiwanPath, reviewPath, photographyPath, culturePath, intlPath } = categoryPath
     let backgroundColor = '#FFF'
     let logo = '/asset/logo.png'
+    let burgerIconClass = 'nav-icon'
+    let mobileMenuClass = 'mobile-link-container'
     if (this.props.bgStyle === 'dark') {
       backgroundColor = '#3e3a39'
       logo = 'asset/logo_dark.png'
+    }
+    if (this.state.open) {
+      burgerIconClass = 'nav-icon open'
+      mobileMenuClass = 'mobile-link-container open'
+    } else {
+      burgerIconClass = 'nav-icon'
+      mobileMenuClass = 'mobile-link-container'
     }
 
     return (
@@ -28,6 +42,19 @@ export default class MobileNaviBar extends Component {
         <div style= {{ backgroundColor: backgroundColor }}>
           <div className="nav-logo">
             <Link to="/"><img src={logo} /></Link>
+          </div>
+          <div className={burgerIconClass} onClick={()=> { this.setState( { open: !this.state.open } )}}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={mobileMenuClass}>
+            <Link to={taiwanPath}><h1>台灣</h1></Link>
+            <Link to={intlPath}><h1>國際兩岸</h1></Link>
+            <Link to={culturePath}><h1>文化</h1></Link>
+            <Link to={photographyPath}><h1>影像</h1></Link>
+            <Link to={reviewPath}><h1>專欄</h1></Link>
           </div>
         </div>
       </Sticky>
