c5ff6823dd0760a80f8b9dbee371238fa0eb597a
nickhsine <nickhsine97753017@gmail.com>
2016-02-05 15:15:17 +0800

    enabel isomorphic

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 /*eslint no-console: 0*/
+/*global __DEVELOPMENT__ webpackIsomorphicTools */
 import Compression from 'compression'
 import Express from 'express'
 import path from 'path'
@@ -16,7 +17,7 @@ import crateRoutes from '../src/routes/index'
 
 import { Provider } from 'react-redux'
 import DeviceProvider from '../src/components/DeviceProvider'
-import config from '../config'
+import config from './config'
 
 const server = new Express()
 const targetUrl = 'http://' + config.apiHost + ':' + config.apiPort
@@ -68,6 +69,11 @@ proxy.on('error', (error, req, res) => {
 })
 
 server.get('*', (req, res) => {
+  if (__DEVELOPMENT__) {
+    // Do not cache webpack stats: the script file would change since
+    // hot module replacement is enabled in the development env
+    webpackIsomorphicTools.refresh()
+  }
   let history = createMemoryHistory()
   let store = configureStore()
 
@@ -111,9 +117,17 @@ server.get('*', (req, res) => {
             </DeviceProvider>
           </Provider>
         )
+        let assets = webpackIsomorphicTools.assets()
+        {/* styles (will be present only in production with webpack extract text plugin) */}
+        let styles = ''
+        {
+          Object.keys(assets.styles).map((style, key) => {
+            styles += ReactDOMServer.renderToString(<link href={assets.styles[style]} key={key} media="screen, projection" rel="stylesheet" type="text/css" charSet="UTF-8"/>)
+          }
+        )}
 
         if ( getCurrentUrl() === reqUrl ) {
-          res.render('index', { html, reduxState })
+          res.render('index', { html, reduxState, styles, javascript: assets.javascript.main })
         } else {
           res.redirect(302, getCurrentUrl())
         }
@@ -135,5 +149,17 @@ server.get('*', (req, res) => {
     ]
   }
 })
+
+if (config.port) {
+  server.listen(config.port, (err) => {
+    if (err) {
+      console.error(err)
+    }
+    console.info('----\n==> ✅  %s is running, talking to API server on %s.', config.app.title, config.apiPort)
+    console.info('==> 💻  Open http://%s:%s in a browser to view the app.', config.host, config.port)
+  })
+} else {
+  console.error('==>     ERROR: No PORT environment variable has been specified')
+}
 
 module.exports = server
