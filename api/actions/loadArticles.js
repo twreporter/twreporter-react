4716d4355228f37d84eece1fc5c802c1b0f14213
garfieldduck <garfieldduck@gmail.com>
2016-05-20 18:17:40 +0800

    add request to get remote images when loading a specific article

--- api/actions/loadArticles.js
+++ api/actions/loadArticles.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import superAgent from 'superagent'
 import config from '../config'
 
@@ -14,7 +15,28 @@ export default function loadArticles(req, params = []) {
       if (err) {
         reject(err)
       } else {
-        resolve(res.body)
+        console.log('\n ***QUERY', query, url)
+        const queryObj = JSON.parse(_.get(query, 'embedded'))
+        const aricleRes = res.body
+        const writers =  _.get(aricleRes, 'writters')
+        if(_.get(queryObj, 'authorImages')) {
+          // combine author images data if the query contains 'authorImages'
+          const imgIds = _.uniq(_.map(writers, (writers)=>{ return '"' + writers.image + '"' }))
+          const imgQuery = encodeURI(`where={"_id":{"$in":[${imgIds}]}}`)
+          const imgUrl = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/images?${imgQuery}`
+          console.log('\n ***WRITERS', imgQuery, imgUrl)
+
+          superAgent['get'](imgUrl).timeout(500)
+          .end(function (err, res) {
+            if (err) {
+              reject(err)
+            } else {
+              console.log('\n ***IMAGES', res.body)
+            }
+          })
+        } else {
+          resolve(aricleRes)
+        }
       }
     })
   })
