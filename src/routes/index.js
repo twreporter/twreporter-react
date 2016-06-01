c7929a8b50369f1e52c4d0deca7c92834a0d048a
ChienHsin-Chan <hcchien@gmail.com>
2016-06-01 11:25:05 +0800

    add a new page for health check

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Router, Route, IndexRoute } from 'react-router'
 
 import Article from '../containers/Article'
+import Blank from '../containers/Blank'
 import Category from '../containers/Category'
 import Home from '../containers/Home'
 import NavBar from '../components/NavBar'
@@ -51,6 +52,7 @@ export default function (history) {
         <IndexRoute component={Home} />
         <Route path="category/:category" component={Category}/>
         <Route path="photography" component={Photography}/>
+        <Route path="check" component={Blank}/>
         <Route path="a/:slug" component={Article}/>
       </Route>
     </Router>
