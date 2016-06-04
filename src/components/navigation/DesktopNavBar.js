da028b8761f9d42be4c51cf4e00de9c183e259d9
garfieldduck <garfieldduck@gmail.com>
2016-06-04 17:23:45 +0800

    first version of workable desktop navigation bar - Destop navigation bar for the article page - add color for the tabs of mobile browsers

--- src/components/navigation/DesktopNavBar.js
+++ src/components/navigation/DesktopNavBar.js
@@ -1,75 +1,55 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import classNames from 'classnames'
-import { navPath } from '../../lib/constants'
-import SubNavBar from './SubNavBar'
-import SearchBox from './SearchBox'
+import styles from './DesktopNavBar.scss'
 
 if (process.env.BROWSER) {
   require('../../containers/NavBar.css')
 }
 
-class Items extends Component {
-  render() {
-    const { children, path, bgStyle } = this.props
-    let style = {
-      borderColor: bgStyle === 'dark' ? '#FFF' : '#000'
-    }
-    let _children = []
-    children.map((child, i) => {
-      let itemClassName
-      if (child.props.to === path) {
-        itemClassName = 'selected-menu-item'
-      }
-      _children.push(
-        <li className={classNames('menu-item', itemClassName)} style={style} key={i}>
-          {child}
-        </li>
-      )
-    })
-    return (
-      <ul className="menu-items">
-        {_children}
-      </ul>
-    )
-  }
-}
-
 export default class DesktopNavBar extends Component {
   constructor(props, context) {
     super(props, context)
+    this.state = {
+      open: false
+    }
   }
   render() {
-    const { bgStyle, path } = this.props
+    const { bgStyle } = this.props
     let backgroundColor = '#FFF'
-    let color = '#000'
-    let logo = '/asset/logo.png'
-    let navLinks = []
+    let logo = '/asset/logo-desk.svg'
+
+    let burgerIconClass = styles.navIcon
 
     if (bgStyle === 'dark') {
       backgroundColor = '#3e3a39'
-      color = '#FFF'
-      logo = 'asset/logo_dark.png'
+      logo = '/asset/logo-desk-dark.svg'
     }
 
-    for (let i in navPath) {
-      navLinks.push(<Link key={i} style={{ color: color }} to={navPath[i].path}><h1>{navPath[i].title}</h1></Link>)
+    if (this.state.open) {
+      burgerIconClass = classNames(styles.navIcon, styles.iconOpen)
     }
 
     return (
       <div className="nav-menu" style={{ backgroundColor: backgroundColor }}>
-        <div className="nav-logo-category">
-          <div className="nav-logo">
-            <Link to="/"><img src={logo} height="50px" width="auto" /></Link>
-            <div className="nav-category">
-              <Items path={path} bgStyle={bgStyle}>
-                {navLinks}
-              </Items>
+        <div className={styles.navContainer}>
+          <div className={styles.navLeft}>
+            <div className={burgerIconClass} onClick={()=> { this.setState( { open: !this.state.open } )}}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
-            <SearchBox class="searchbox" style={{ width: '230px', display: path !== '/photography' ? 'inline-block' : 'none' }} path={path} />
+          </div>
+          <div className={styles.navCenter}>
+            <Link className={styles.navLogo} to="/"><img src={logo} /></Link>
+          </div>
+          <div className={styles.navRight}>
+            <a target="_blank" className={styles.donateButton} href="https://twreporter.backme.tw/cashflow/checkout?project_id=175&reward_id=718">
+              <img className={styles.donateIcon} src="/asset/donate.svg"/>贊助我們
+            </a>
           </div>
         </div>
-        {/*<SubNavBar {...this.props}/>*/}
       </div>
     )
   }
