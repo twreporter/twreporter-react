3bdaf5fdbedea8a2e435c301ce391da7164bb082
nickhsine <nickhsine97753017@gmail.com>
2016-06-20 19:19:41 +0800

    Update InfoBox and Paragraph components

--- src/components/article/Paragraph.js
+++ src/components/article/Paragraph.js
@@ -4,4 +4,6 @@ import classNames from 'classnames'
 // import styles from './Paragraph.scss'
 import React from 'react' // eslint-disable-next-line
 
-export const Paragraph = ({ content }) => <p className={classNames('inner-max', 'center-block')}>{ _.get(content, [ 0 ], '') }</p>
+export const Paragraph = ({ content }) => {
+  return <div className={classNames('inner-max', 'center-block')} dangerouslySetInnerHTML={{ __html: _.get(content, [ 0 ], '') }} />
+}
