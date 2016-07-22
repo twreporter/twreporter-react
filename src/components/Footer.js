30e3bc2addfba979c3a2a38f7e428596e888388b
Mika <garfieldduck@gmail.com>
2016-07-22 17:10:41 +0800

    Refine footer styles
    Refine footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import { copyrightTypes, donatePath } from '../constants/index'
+import classNames from 'classnames'
+import logoFB from '../../static/asset/icon-facebook.svg'
+import logoGithub from '../../static/asset/icon-github.svg'
+import logoIcon from '../../static/asset/logo-mobile.svg'
+import logoIG from '../../static/asset/icon-instagram.svg'
+import logoLine from '../../static/asset/icon-line.svg'
+import logoRss from '../../static/asset/icon-rss.svg'
 import React, { Component } from 'react'
 import styles from './Footer.scss'
-import logoIcon from '../../static/asset/logo-mobile.svg'
 
 export default class Footer extends Component {
   constructor(props) {
@@ -28,9 +34,10 @@ export default class Footer extends Component {
     return (
       <div className={styles['footer']}>
         <div className="container inner-max">
+
           <div className="row">
             <div className="col-md-12">
-              <div className="logo-container">
+              <div className={styles['logo-container']}>
                 <div className={styles['logo']}>
                   <a href="/" target="_self">
                       <img
@@ -42,55 +49,65 @@ export default class Footer extends Component {
               </div>
             </div>
           </div>
-          <div className="us-container row text-center">
-            <div className="item col-xs-3">
+
+          <div className={classNames('row', 'text-center', styles['us-container'])}>
+            <div>
               <a href="/a/about-us-footer">
                   關於我們
               </a>
             </div>
-            <div className="item col-xs-3">
+            <div>
               <a href="/a/contact-footer">
                   聯絡我們
               </a>
             </div>
-            <div className="item col-xs-3">
+            <div>
               <a href="/a/privacy-footer">
                   隱私政策
               </a>
             </div>
-            <div className="item col-xs-3">
+            <div className={styles['support']}>
               <a href={donatePath} target="_blank">
                   贊助我們
               </a>
             </div>
           </div>
-        </div>
 
-        <div className="row text-center">
-          <div className={styles['social-container']}>
-                <div className="item">
+          <div className="row text-center">
+            <div className={styles['social-container']}>
+                <div className={styles['item']}>
                   <a href="https://www.facebook.com/twreporter/" target="_blank">
-                      <img className="fb" src="/asset/FB"/>
+                      <img src={ logoFB }/>
                   </a>
                 </div>
-                <div className="item">
+                <div className={styles['item']}>
                   <a href="https://www.instagram.com/twreporter/" target="_blank" >
-                      <img className="ig" src="/asset/IG.png" />
+                      <img src={logoIG} />
+                  </a>
+                </div>
+                <div className={styles['item']}>
+                  <a href="http://line.me/ti/p/%40nbs5015j" target="_blank" >
+                      <img src={logoLine} />
+                  </a>
+                </div>
+                <div className={styles['item']}>
+                  <a href="https://github.com/twreporter" target="_blank" >
+                      <img src={logoGithub} />
+                  </a>
+                </div>
+                <div className={styles['item']}>
+                  <a href="https://www.twreporter.org/a/rss2.xml" target="_blank" >
+                      <img src={logoRss} />
                   </a>
                 </div>
               </div>
+          </div>
         </div>
 
         <div className="open-source-container">
           <div className="container inner-max">
               <div className="row text-center">
                 {copyrightLink}
-              </div>
-              <div className="item">
-                <a href="https://github.com/twreporter" target="_blank">
-                  <img src="/asset/github.png" className="img" />
-                  <span> github.com/twreporter</span>
-                </a>
               </div>
           </div>
         </div>
