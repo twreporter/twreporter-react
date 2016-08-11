a081d746ae7518764b47300de06d095ccadf7fa4
ChienHsin-Chan <hcchien@gmail.com>
2016-08-11 16:05:33 +0800

    try to fix the webpack issues for the react-flex-carousel

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -51,7 +51,15 @@ module.exports = {
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-      { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
+      { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' },
+      {
+        test: /\.js$/,
+        include: /react-flex-carousel/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015"]
+        }
+      }
     ]
   },
   postcss: [autoprefixer],
