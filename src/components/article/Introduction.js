c97a097738536f6461d2e32a2d5b57718295dfe2
Mika <garfieldduck@gmail.com>
2016-08-16 21:35:20 +0800

    Enable HTML rendering function in the Introduction
    - enable HTML rendering function in the Introduction
    - hide donation text on tablet when users scroll down the menu

--- src/components/article/Introduction.js
+++ src/components/article/Introduction.js
@@ -9,28 +9,30 @@ import React, { Component } from 'react'
 export const Introduction = ({ data }) => {
   let content
   let block = false
+  let introArr = []
  
   if (Array.isArray(data)) {
     content = data.map((ele, idx) => {
       let element = _.get(ele, [ 'content', 0 ])
       if (element.length > 0) {
         block = true
-        return (
-          <p key={ele.id || idx}>{_.get(ele, [ 'content', 0 ])}</p>
-        )
+        let paragraph = _.get(ele, [ 'content', 0 ])
+        introArr.push(<p key={ele.id || idx} dangerouslySetInnerHTML={{ __html: paragraph }}></p>)
+        return paragraph
       }
     } )
   } else {
     if (content.length > 0) {
       block = true
       content = "<p>{_.get(data, [ 'content', 0 ])}</p>"
+      introArr.push(<div dangerouslySetInnerHTML={{ __html: content }} />)
     } 
   }
 
   if (block) {
     return (
       <div className={classNames(styles['intro-container'], 'text-justify', commonStyles['text-color'])}>
-        {content}
+      {introArr}
       </div>
     )
   } else {
