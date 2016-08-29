e088777d9ad8a5e11e23dbc74efaa7a0db9094e8
Mika <garfieldduck@gmail.com>
2016-08-29 15:02:19 +0800

    adjust characters limits of the article header
    adjust characters limits of the article header

--- src/constants/index.js
+++ src/constants/index.js
@@ -61,7 +61,8 @@ export const copyrightTypes = {
 
 export const CHARACTERS_LIMIT = {
   TOPIC_DESC: 120,
-  BOTTOM_RELATED_DESC: 120
+  BOTTOM_RELATED_DESC: 120,
+  HEADER_TITLE_TRIMMED_RATIO: 0.45
 }
 
 export const ITEMS_LIMIT = {
