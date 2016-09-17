6e393346e9b176124fdf32685dc1c86e061fadb8
nickhsine <nickhsine97753017@gmail.com>
2016-09-17 14:55:59 +0800

    LeadingVideo component - render video when browser width is bigger than desktop - render heroImage when browser width is less than desktop - not to render LeadingImage when LeadingVideo is existed

---
+++ src/components/article/LeadingVideo.js
@@ -0,0 +1,139 @@
+/* eslint no-unused-vars:0 */
+'use strict'
+import screenSize from '../../constants/screen-size'
+import { replaceStorageUrlPrefix } from '../../utils/index'
+import _ from 'lodash'
+import VideoPlayer from 'react-player'
+import React, { Component } from 'react' // eslint-disable-line
+import ReactDOM from 'react-dom'
+import cx from 'classnames'
+import styles from './LeadingVideo.scss'
+import vjs from 'video.js'
+
+if (process.env.BROWSER) {
+  require ('./LeadingVideo.css')
+}
+
+const DEFAULT_VIDEO_OPTIONS = {
+  preload: 'auto',
+  autoplay: true,
+  controls: false
+}
+
+class LeadingVideo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.mountVideoPlayer()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let currentSrc = this.props.src
+    let newSrc = nextProps.src
+
+    if (currentSrc !== newSrc) {
+      this.setVideoPlayerSrc(replaceStorageUrlPrefix(newSrc))
+    }
+  }
+
+  componentWillUnmount() {
+    this.unmountVideoPlayer()
+  }
+
+  getVideoPlayer() {
+    return this._player
+  }
+
+  getVideoPlayerEl() {
+    return ReactDOM.findDOMNode(this.refs.videoPlayer)
+  }
+
+  getVideoPlayerOptions() {
+    const { poster } = this.props
+
+    return _.defaults(
+      {}, this.props.options, { poster: _.get(poster, 'desktop.url', '') }, DEFAULT_VIDEO_OPTIONS)
+  }
+
+  setVideoPlayerSrc(src) {
+    this._player.src(src)
+  }
+
+  mountVideoPlayer() {
+    let src = replaceStorageUrlPrefix(this.props.src)
+    let options = this.getVideoPlayerOptions()
+    this._player = vjs(this.getVideoPlayerEl(), options)
+    let player = this._player
+    player.ready()
+    player.src(src)
+  }
+
+  unmountVideoPlayer() {
+    this._player.dispose()
+  }
+
+  pauseVideo() {
+    this._player.pause()
+  }
+
+  playVideo() {
+    this._player.play()
+  }
+
+  restartVideo() {
+    this._player.currentTime(0).play()
+  }
+
+  togglePauseVideo() {
+    if (this._player.paused()) {
+      this.playVideo()
+    } else {
+      this.pauseVideo()
+    }
+  }
+
+  _composeSrcSet(imgObj) {
+    let desktopSrc = replaceStorageUrlPrefix(_.get(imgObj, [ 'desktop', 'url' ]))
+    let tabletSrc = replaceStorageUrlPrefix(_.get(imgObj, [ 'tablet', 'url' ]))
+    let mobileSrc = replaceStorageUrlPrefix(_.get(imgObj, [ 'mobile', 'url' ]))
+    return `${mobileSrc} ${screenSize.smallScreenMinWidth}w, ${tabletSrc} ${screenSize.mediumScreenMinWidth}w, ${desktopSrc} ${screenSize.largeScreenMinWidth}w`
+  }
+
+  render() {
+    const { poster } = this.props
+    let videoPlayerClasses = cx({
+      'video-js': true,
+      [styles['leading-video']]: true
+    })
+
+    let imageSrcSet = this._composeSrcSet(poster)
+    let imageSrc = replaceStorageUrlPrefix(_.get(poster, 'mobile.url', ''))
+
+    return (
+      <div>
+        <video ref="videoPlayer" className={videoPlayerClasses}>
+        </video>
+        <img
+          className={styles['leading-poster']}
+          src={imageSrc}
+          srcSet={imageSrcSet}
+        />
+      </div>
+    )
+  }
+}
+
+LeadingVideo.propTypes = {
+  options: React.PropTypes.object,
+  poster: React.PropTypes.object,
+  src: React.PropTypes.string.isRequired
+}
+
+LeadingVideo.defaultProps = {
+  options: DEFAULT_VIDEO_OPTIONS,
+  poster: {}
+}
+
+export { LeadingVideo }
