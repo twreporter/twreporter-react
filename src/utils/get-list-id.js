ae6da5be83ee387566acf09ef1f4427ac6701545
nickhsine <nickhsine97753017@gmail.com>
2016-10-12 23:03:00 +0800

    Change sub navigation

--- src/utils/get-list-id.js
+++ src/utils/get-list-id.js
@@ -1,6 +1,6 @@
 /*global __DEVELOPMENT__ */
 'use strict'
-import { devCatListId, devTopicListId, prodCatListId, prodTopicListId } from '../conf/list-id'
+import { devCatListId, prodCatListId } from '../conf/list-id'
 
 function getCatId(catName) {
   if (__DEVELOPMENT__) {
@@ -9,14 +9,6 @@ function getCatId(catName) {
   return prodCatListId[catName]
 }
 
-function getTopicId(topicName) {
-  if (__DEVELOPMENT__) {
-    return devTopicListId[topicName]
-  }
-  return prodTopicListId[topicName]
-}
-
 export {
-  getCatId,
-  getTopicId
+  getCatId
 }
