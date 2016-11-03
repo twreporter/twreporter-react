7a11dd4bd9a36542a130306289574b107b4e7f68
nickhsine <nickhsine97753017@gmail.com>
2016-11-03 17:24:52 +0800

    [Update] Make error message as constants and render error message nicely

--- src/components/SystemError.js
+++ src/components/SystemError.js
@@ -1,5 +1,11 @@
 'use strict'
 import React from 'react'
+import commonStyles from './article/Common.scss'
+import cx from 'classnames'
+import refreshBt from '../../static/asset/refresh_bt.svg'
+import styles from './SystemError.scss'
+import { ERROR_MESSAGE_404, ERROR_MESSAGE_500 } from '../constants/index'
+
 // lodash
 import get from 'lodash/get'
 
@@ -8,16 +14,31 @@ const _ = {
 }
 
 function SystemError({ error }) {
+  const refreshPage = () => {
+    // on client side
+    if (window) {
+      window.location.reload()
+    }
+  }
+
   let style = {
     marginTop: 35
   }
+
   let errorMessageJsx = _.get(error, 'status', 500) === 404 ? (
-    <h1 style={style}>抱歉，網頁不存在，請確認網址是否正確。</h1>
+    <h1 style={style}>{ERROR_MESSAGE_404}</h1>
   ) : (
-    <h1 style={style}>抱歉，系統繁忙中，請重新整理頁面。</h1>
+    <div className={commonStyles['inner-block']}>
+      <div className={cx('center-block', 'text-center', styles.message)}>
+        {ERROR_MESSAGE_500}
+      </div>
+        <div className={cx(styles['refresh-bt'], 'text-center')}>
+          <img src={refreshBt} width="30px" height="30px" onClick={refreshPage} style={{ cursor: 'pointer' }}/>
+        </div>
+    </div>
   )
   return (
-    <div className="container">
+    <div className="container" style={{ height: 300, marginTop: 50 }}>
       {errorMessageJsx}
     </div>
   )
