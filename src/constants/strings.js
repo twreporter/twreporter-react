0f7cf5efd87c5ca49c2f2562ad6d7b5b5c3ae50d
yucj <nakts0123@gmail.com>
2016-11-18 15:03:47 +0800

    update load more authors constant

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -6,7 +6,7 @@ const MEDIA_CH_STR = '媒體'
 const REVIEW_CH_STR = '評論'
 const SPECIAL_TOPIC_CH_STR = '專題'
 const TAIWAN_CH_STR = '台灣'
-const LOAD_MORE = '載入更多'
+const LOAD_MORE = '載入更多作者'
 
 export {
   CULTURE_CH_STR,
