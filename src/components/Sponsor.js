873ba0f8dfd1dccf8ea229cc2f6f1722e0218a33
yucj <nakts0123@gmail.com>
2016-11-22 18:39:09 +0800

    Update the styles with RWD

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import styles from './authors/AuthorList.scss'
 import { SPONSOR_TITLE, SPONSOR_TEXT, SPONSOR_BUTTON } from '../constants/sponsor'
+import { donatePath } from '../constants/index'
 import map from 'lodash/map'
+import classNames from 'classnames'
+import styles from './Sponsor.scss'
 
 const _ = {
   map
 }
 
-const Sponsor = () =>{
-  return (<div className={styles['sponsor-box']}>
-    <div className={styles['sponsor-title']}>{SPONSOR_TITLE}</div>
-    <div className={styles['sponsor-text']}>{_.map(SPONSOR_TEXT, (text,index) => <p key={index}>{text}</p>) }</div>
-    <div className={styles['sponsor-button']}>{SPONSOR_BUTTON}</div>
-  </div>)
+const boxClasses = classNames(
+  styles['sponsor-box'],
+  'center-block'
+)
+
+const textClasses = classNames(
+  styles['sponsor-text'],
+  'center-block'
+)
+
+const buttonClasses = classNames(
+  styles['sponsor-button']
+)
+
+class Sponsor extends React.Component {
+  render() {
+    return (<div className={boxClasses}>
+      <div className={styles['sponsor-title']}>{SPONSOR_TITLE}</div>
+      <div className={textClasses}>{_.map(SPONSOR_TEXT, (text,index) => <p key={index}>{text}</p>) }</div>
+      <a href={donatePath} target="_blank" className={buttonClasses}>{SPONSOR_BUTTON}</a>
+    </div>)
+  }
 }
 
 export default Sponsor
