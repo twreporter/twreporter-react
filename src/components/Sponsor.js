7d22d8b73be8d9dea186ae72441b491d5f76f40d
yucj <nakts0123@gmail.com>
2016-12-01 11:22:54 +0800

    [Sponsor] Modify sponsor box styles

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -10,13 +10,13 @@ const _ = {
 }
 
 const boxClasses = classNames(
-  styles['sponsor-box'],
-  'center-block'
+  'center-block',
+  styles['sponsor-box']
 )
 
 const textClasses = classNames(
-  styles['sponsor-text'],
-  'center-block'
+  'center-block',
+  styles['sponsor-text']
 )
 
 const buttonClasses = classNames(
@@ -25,10 +25,13 @@ const buttonClasses = classNames(
 
 class Sponsor extends React.Component {
   render() {
-    return (<div className={boxClasses}>
-      <div className={styles['sponsor-title']}>{SPONSOR_TITLE}</div>
-      <div className={textClasses}>{_.map(SPONSOR_TEXT, (text,index) => <p key={index}>{text}</p>) }</div>
-      <a href={donatePath} target="_blank" className={buttonClasses}>{SPONSOR_BUTTON}</a>
+    return (
+    <div>
+      <div className={boxClasses}>
+        <div className={styles['sponsor-title']}>{SPONSOR_TITLE}</div>
+        <div className={textClasses}>{_.map(SPONSOR_TEXT, (text,index) => <p key={index}>{text}</p>) }</div>
+        <a href={donatePath} target="_blank" className={buttonClasses}>{SPONSOR_BUTTON}</a>
+      </div>
     </div>)
   }
 }
