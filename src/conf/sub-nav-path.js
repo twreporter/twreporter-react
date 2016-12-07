51a158b0d92152c758767c59a8b29316dc0b07b5
nickhsine <nickhsine97753017@gmail.com>
2016-12-07 15:29:40 +0800

    [Update] delete irregular whitespace which breaks lint

--- src/conf/sub-nav-path.js
+++ src/conf/sub-nav-path.js
@@ -1,5 +1,3 @@
-﻿'use strict'
-
 export default [ {
   title: '零安樂死真相',
   path: '/a/protect-animal-no-killing-ch1'
@@ -8,17 +6,18 @@ export default [ {
   title: '一例一休吵什麼？',
   path: '/a/taiwan-labor-day-off-dispute'
 },
-{ title: '美國總統大選',
+{
+  title: '美國總統大選',
   path: '/tag/57b065e5360b651200848d76'
 },
 {
   title: '台灣空汙紫爆怎麼辦',
   path: '/tag/5773c909c107b51000e9beb4'
-},
+}, 
 {
   title: '走入同志家庭',
   path: '/topic/57ac8177363d1610007ef659'
-},
+}, 
 {
   title: '轉型正義',
   path: '/topic/57ac8151363d1610007ef656'
