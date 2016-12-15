c672b46f8e220250e94b04885370931b4dbe8439
nickhsine <nickhsine97753017@gmail.com>
2016-12-15 17:06:45 +0800

    [Add] Add PromotionBanner component

---
+++ src/components/shared/PromotionBanner.js
@@ -0,0 +1,36 @@
+'use strict'
+import styles from './PromotionBanner.scss'
+import React, { PropTypes } from 'react' // eslint-disable-next-line
+
+export default function PromotionBanner({ headline, iconImgSrc, bgImgSrc, subtitle, title }) {
+  const bgStyle = {
+    background: bgImgSrc ? `url(${bgImgSrc}) no-repeat center center` : '',
+    backgroundSize: 'cover'
+  }
+  return (
+    <div className={styles.container} style={bgStyle}>
+      { headline ? <div className={styles.headline}>{headline}</div> : null}
+      <div className={styles['title-block']}>
+        { iconImgSrc ? <img src={iconImgSrc} /> : null }
+        <span>{title}</span>
+      </div>
+      <div>{subtitle}</div>
+    </div>
+  )
+}
+
+PromotionBanner.propTypes = {
+  headline: PropTypes.string,
+  iconImgSrc: PropTypes.string,
+  bgImgSrc: PropTypes.string,
+  subtitle: PropTypes.string,
+  title: PropTypes.string
+}
+
+PromotionBanner.defaultProps = {
+  headline: '',
+  iconImgSrc: '',
+  bgImgSrc: '',
+  subtitle: '',
+  title: ''
+}
