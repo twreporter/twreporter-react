52e5b6dbeff9b0adab5a7d0fae877b4e9231fa9b
yucj <nakts0123@gmail.com>
2016-12-15 16:49:45 +0800

    Add searching event

--- src/components/authors/AuthorSearchBox.js
+++ src/components/authors/AuthorSearchBox.js
@@ -1,29 +1,77 @@
 import React from 'react'
 import { SEARCHING_AUTHOR_NAME } from '../../constants/authors-list'
-import searchIcon from '../../../static/asset/search.svg'
 import resetIcon from '../../../static/asset/reset.svg'
+import searchIcon from '../../../static/asset/search.svg'
 import styles from './AuthorSearchBox.scss'
 
-const AuthorSearchBox = () => {
+class AuthorSearchBox extends React.Component {
 
-  function _handleButton() {
-    event.preventDefault()
-    console.log('click') // eslint-disable-line
+  constructor(props) {
+    super(props)
+    this.state = this.initialState = { keywords: '' }
+    this._handleClickButton = this._handleClickButton.bind(this)
+    this._handleSubmit = this._handleSubmit.bind(this)
+    this._handleChange = this._handleChange.bind(this)
   }
 
-  return (<div>
-    <div className={styles['searchbox-wrapper']}>
-      <form noValidate="novalidate" onSubmit={()=>{return false}} className={styles['searchbox']}>
-        <input type="search" name="searchAuthor" placeholder={SEARCHING_AUTHOR_NAME} className={styles['input-box']} required="required" />
-        <button type="button" className={styles['submit-button']} onClick={_handleButton.bind(this)}>
-          <img src={searchIcon}/>
-        </button>
-        <button type="reset" title="Clear the search query." className={styles['reset-button']}>
-          <img src={resetIcon}/>
-        </button>
-      </form>
-    </div>
-  </div>)
+  _handleChange(event) {
+    event.preventDefault()
+    const input = event.target.value
+    if (!input) return this.setState(this.initialState)
+    this.setState({ keywords: input })
+    console.log('change')// eslint-disable-line
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault()
+    console.log('submit')// eslint-disable-line
+    console.log(this.state.keywords)// eslint-disable-line
+  }
+
+  _handleClickButton(event) {
+    event.preventDefault()
+    console.log('click') // eslint-disable-line
+    console.log(this.state.keywords)// eslint-disable-line
+  }
+
+  // _refCallback(input) {
+  //   console.log(this)
+  //   console.log(input)
+  // }
+
+  render() {
+    return (<div>
+      <div className={styles['searchbox-wrapper']}>
+        <form noValidate="novalidate"
+          className={styles['searchbox']}
+          onReset={this._handleChange}
+          onSubmit={this._handleSubmit} >
+          <input
+            type="search"
+            className={styles['input-box']}
+            name="searchAuthor"
+            value={this.state.keywords}
+            placeholder={SEARCHING_AUTHOR_NAME}
+            onChange={this._handleChange}
+            autoComplete="off"
+            required="required" />
+          <button
+            type="button"
+            className={styles['submit-button']}
+            onClick={this._handleClickButton}>
+            <img src={searchIcon}/>
+          </button>
+          <button
+            type="reset"
+            className={styles['reset-button']}
+            title="Clear the search query."
+            onClick={this._handleChange}>
+            <img src={resetIcon}/>
+          </button>
+        </form>
+      </div>
+    </div>)
+  }
 }
 
 export default AuthorSearchBox
