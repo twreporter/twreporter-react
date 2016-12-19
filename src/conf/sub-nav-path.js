1ed80a2369f782a988e2db6ab805174398027ee8
nickhsine <nickhsine97753017@gmail.com>
2016-12-19 17:01:00 +0800

    [Update] Update sub-nav-path.js

--- src/conf/sub-nav-path.js
+++ src/conf/sub-nav-path.js
@@ -1,4 +1,7 @@
 export default [ {
+  title: '血淚漁場',
+  path: '/topics/far-sea-fishing-investigative-report'
+},{
   title: '零安樂死真相',
   path: '/a/protect-animal-no-killing-ch1'
 },
@@ -7,17 +10,13 @@ export default [ {
   path: '/a/taiwan-labor-day-off-dispute'
 },
 {
-  title: '美國總統大選',
-  path: '/tag/57b065e5360b651200848d76'
-},
-{
   title: '台灣空汙紫爆怎麼辦',
   path: '/tag/5773c909c107b51000e9beb4'
-}, 
+},
 {
   title: '走入同志家庭',
   path: '/topic/57ac8177363d1610007ef659'
-}, 
+},
 {
   title: '轉型正義',
   path: '/topic/57ac8151363d1610007ef656'
