534667c0c00217009a32e4311684532e6df0982e
yucj <nakts0123@gmail.com>
2016-12-22 17:12:19 +0800

    Remove useless constants

--- src/constants/algolia.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-export const APP_ID = 'CTAAU9LLQ2'
-export const SEARCH_API_KEY = '4e4d99db26090df488e481abf441914d'
-export const CONTACTS_INDEX = 'contacts-index'
-export const POSTS_INDEX = 'posts-index'
