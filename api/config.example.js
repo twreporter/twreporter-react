39205829e1d11898b1c05bd3fa55fab7d2cc382b
yucj <nakts0123@gmail.com>
2016-12-23 19:38:03 +0800

    Remove example key

--- api/config.example.js
+++ api/config.example.js
@@ -4,6 +4,6 @@ export default {
   API_PORT: '8080',
   REDIS_HOST: 'localhost',
   REDIS_PORT: '6379',
-  ALGOLIA_APP_ID: 'CTAAU9LLQ2',
-  ALGOLIA_SEARCH_API_KEY: '4e4d99db26090df488e481abf441914d'
+  ALGOLIA_APP_ID: '', // enter your algolia app id
+  ALGOLIA_SEARCH_API_KEY: '' // enter your search api key
 }
