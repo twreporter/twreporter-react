ab494f149ff28afaacf435c3d68d8abc8cec400f
Mika <garfieldduck@gmail.com>
2016-12-28 17:54:30 +0800

    [Update] Add HotJar tracking code, adjust the meta for SEO
    - Integrate HotJar analytics
    - adjust the meta for SEO for the search bar

--- src/components/Html.js
+++ src/components/Html.js
@@ -76,6 +76,20 @@ function Html({ canonical, children, description, meta, reduxState, styles, scri
           <iframe src="//www.googletagmanager.com/ns.html?id=GTM-NB59ZP" height="0" width="0" style={{ display:'none', visibility:'hidden' }}></iframe>
         </noscript>
         {/* <!-- End Google Tag Manager (noscript) --> */}
+        {/* <!-- Hotjar Tracking Code for https://www.twreporter.org/ --> */}
+        <script
+          dangerouslySetInnerHTML={{ __html:
+            `(function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:376929,hjsv:5};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');`
+          }}
+        />
+        {/* <!-- End - Hotjar Tracking Code for https://www.twreporter.org/ --> */}
       </body>
     </html>
   )
