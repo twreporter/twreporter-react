5c6dde41bb5a571490080280e6b8c15e33a311ee
Mika <garfieldduck@gmail.com>
2017-01-04 15:50:00 +0800

    [Update] Replace Google tag manager with react-ga
    - Let Google analytics track router changes accurately

--- src/components/Html.js
+++ src/components/Html.js
@@ -42,17 +42,6 @@ function Html({ canonical, children, description, meta, reduxState, styles, scri
         { _.map(styles, (style, key) => {
           return <link async href={style} key={key} media="screen, projection" rel="stylesheet" type="text/css" charSet="UTF-8"/>
         }) }
-        {/* <!-- Google Tag Manager -->*/}
-        <script
-          dangerouslySetInnerHTML={{ __html:
-            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NB59ZP');`
-          }}
-        />
-        {/* <!-- End Google Tag Manager -->*/}
       </head>
       <body>
         <div id="root" dangerouslySetInnerHTML={{ __html: children }} />
@@ -71,11 +60,6 @@ function Html({ canonical, children, description, meta, reduxState, styles, scri
             'try{Typekit.load({ async: true });}catch(e){}'
           }}
         />
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
-          <iframe src="//www.googletagmanager.com/ns.html?id=GTM-NB59ZP" height="0" width="0" style={{ display:'none', visibility:'hidden' }}></iframe>
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
         {/* <!-- Hotjar Tracking Code for https://www.twreporter.org/ --> */}
         <script
           dangerouslySetInnerHTML={{ __html:
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import App from '../containers/App'
@@ -14,9 +15,25 @@ import Tag from '../containers/Tag'
 import Topic from '../containers/Topic'
 import TopicLandingPage from '../containers/TopicLandingPage'
 
+
+if (typeof window !== 'undefined') {
+  // add Google Analytics
+  ReactGA.initialize('UA-69336956-1')
+  ReactGA.set({ page: window.location.pathname })
+}
+
+
+function scrollAndFireTracking() {
+  if(window) {
+    window.scrollTo(0, 0)
+    // send Google Analytics Pageview event on router changed
+    ReactGA.pageview(window.location.pathname)
+  }
+}
+
 export default function (history = browserHistory) {
   return (
-    <Router history={history} onUpdate={() => window.scrollTo(0, 0)}>
+    <Router history={history} onUpdate={scrollAndFireTracking} >
       <Route path="/topics/:slug" component={TopicLandingPage} />
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
