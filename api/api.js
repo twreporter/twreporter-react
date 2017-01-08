32e9197c8aa833b77b9abaf84580a3faa1ed99de
nickhsine <nickhsine97753017@gmail.com>
2017-01-08 17:21:55 +0800

    [Update] Update server/server.js. - Add health check endpoint. - Increase expire time of redis record. - Increase max-age of Cache-Control directive.

--- api/api.js
+++ api/api.js
@@ -8,7 +8,7 @@ import express from 'express'
 import redis from 'redis'
 import serverConfig from '../server/config'
 
-const EXPIRE = 600 // ten mins
+const EXPIRE = 3600 // 1 hour
 const pretty = new PrettyError()
 const app = express()
 
