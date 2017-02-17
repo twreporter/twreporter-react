2af7cea22f7ccf4901d41290b5ffb9dee3f81811
yucj <nakts0123@gmail.com>
2017-02-17 15:25:52 +0800

    Update action creators of author

--- src/actions/author.js
+++ src/actions/author.js
@@ -2,19 +2,21 @@
 
 import * as CONSTANTS from '../constants/index'
 
-import { MAX_ARTICLES_PER_FETCH, REQUEST_PAGE_START_FROM, RETURN_DELAY } from '../constants/author-page'
+import { MAX_ARTICLES_PER_FETCH, NUMBER_OF_FIRST_RESPONSE_PAGE, RETURN_DELAY_TIME } from '../constants/author-page'
 import { arrayOf, normalize } from 'normalizr'
 
 import { InternalServerError } from '../lib/custom-error'
 import { article as articleSchema } from '../schemas/index'
 import { camelizeKeys } from 'humps'
 import fetch from 'isomorphic-fetch'
-import { formatUrl } from '../utils/index'
 import get from 'lodash/get'
+import omit from 'lodash/omit'
+import { formatUrl } from '../utils/index'
 import { urlParasToString } from '../utils/url-paras-to-string'
 
 const _ = {
-  get
+  get,
+  omit
 }
 
 export function requestAuthorCollection(authorId) {
@@ -24,27 +26,26 @@ export function requestAuthorCollection(authorId) {
   }
 }
 
-export function failToReceiveAuthorCollection(error, failedAt) {
+export function failToReceiveAuthorCollection(authorId, error) {
   return {
     type: CONSTANTS.FETCH_AUTHOR_COLLECTION_FAILURE,
+    authorId,
     error,
-    failedAt
+    failedAt: Date.now()
   }
 }
 
-export function receiveAuthorCollection({ authorId, items, currentPage, totalResults, receivedAt, totalPages } = {}) {
+export function receiveAuthorCollection(authorId, items, responseContext) {
   return {
     type: CONSTANTS.FETCH_AUTHOR_COLLECTION_SUCCESS,
-    response: items,
     authorId,
-    currentPage,
-    totalResults,
-    totalPages,
-    receivedAt
+    response: items,
+    responseContext,
+    receivedAt: Date.now()
   }
 }
 
-export function fetchAuthorCollection({ targetPage = REQUEST_PAGE_START_FROM, authorId = '', returnDelay = 0 } = {}) {
+export function fetchAuthorCollection({ targetPage, authorId, returnDelay }) {
   return (dispatch, getState) => { // eslint-disable-line no-unused-vars
     const searchParas = {
       keywords: authorId,
@@ -63,47 +64,53 @@ export function fetchAuthorCollection({ targetPage = REQUEST_PAGE_START_FROM, au
         }
         return response.json()
       })
-      .then((content) => {
-        const currentPage = _.get(content, 'page', 0)
-        const totalResults = _.get(content, 'nbHits', 0)
-        const totalPages = _.get(content, 'nbPages', 0)
-        const searchResultItems = _.get(content, 'hits', {})
-        const camelizedJson = camelizeKeys(searchResultItems)
-        const items = normalize(camelizedJson, arrayOf(articleSchema))
-        const receivedAt = Date.now()
-        const returnParas = { authorId, items, currentPage, totalResults, receivedAt, totalPages }
-        // delay for displaying loading spinner
-        function delayDispatch() {
-          return new Promise((resolve, reject)=> { // eslint-disable-line no-unused-vars
-            setTimeout(() => {
-              resolve()
-            }, returnDelay)
-          })
-        }
-        if (returnDelay > 0) {
-          return delayDispatch().then(()=>{
-            return dispatch(receiveAuthorCollection(returnParas))
-          })
-        }
-        return dispatch(receiveAuthorCollection(returnParas))
-      },
-      (error) => {
-        let failedAt = Date.now()
-        return dispatch(failToReceiveAuthorCollection(error, failedAt))
-      })
+      .then(
+        (responseObject) => {
+          const responseItems = _.get(responseObject, 'hits', {})
+          const responseContext = _.omit(responseObject, 'hits', {})
+          const camelizedJson = camelizeKeys(responseItems)
+          const items = normalize(camelizedJson, arrayOf(articleSchema))
+          const returnParas = { authorId, items, responseContext }
+          // delay for displaying loading spinner
+          function delayDispatch() {
+            return new Promise((resolve, reject)=> { // eslint-disable-line no-unused-vars
+              setTimeout(() => {
+                resolve()
+              }, returnDelay)
+            })
+          }
+          if (returnDelay > 0) {
+            return delayDispatch().then(()=>{
+              return dispatch(receiveAuthorCollection(returnParas))
+            })
+          }
+          return dispatch(receiveAuthorCollection(returnParas))
+        },
+      (error) => dispatch(failToReceiveAuthorCollection(error))
+    )
   }
 }
 
 export function fetchAuthorCollectionIfNeeded(authorId) {
   return (dispatch, getState) => {
-    const author = _.get(getState(), [ 'author', authorId ], {})
-    const isFetching = _.get(author, 'isFetching', false)
-    const isFinish = _.get(author, 'isFinish', false)
-    const currentPage = _.get(author, 'currentPage', REQUEST_PAGE_START_FROM - 1)
-    let targetPage = currentPage + 1
-    const returnDelay = currentPage < REQUEST_PAGE_START_FROM ? 0 : RETURN_DELAY
+    const currentState = getState()
+    const articlesDataOfAnAuthor = _.get(currentState, [ 'articlesByAuthor', authorId ], null)
+    // If state.articlesByAuthor[authorId] does not exist:
+    if (articlesDataOfAnAuthor === null) {
+      return dispatch(fetchAuthorCollection({
+        authorId,
+        targetPage: NUMBER_OF_FIRST_RESPONSE_PAGE,
+        returnDelay: 0
+      }))
+    }
+    //  If state.articlesByAuthor[authorId] exists:
+    const { currentPage, isFetching, isFinish } = articlesDataOfAnAuthor
     if(!isFetching && !isFinish) {
-      return dispatch(fetchAuthorCollection({ targetPage, authorId, returnDelay }))
+      return dispatch(fetchAuthorCollection({
+        authorId,
+        targetPage: currentPage + 1,
+        returnDelay: RETURN_DELAY_TIME
+      }))
     }
   }
 }
