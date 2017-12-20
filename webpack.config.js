be86658437ba0db4cb8295e977f086adfae18ac4
nickhsine <nickhsine97753017@gmail.com>
2017-12-20 18:21:31 +0800

    Update processes.json and webpack.config.js. Use production settings

--- webpack.config.js
+++ webpack.config.js
@@ -166,10 +166,10 @@ const webpackConfig = {
       'process.env': {
         BROWSER: true,
         NODE_ENV: isProduction ? '"production"' : '"development"',
-        RELEASE_BRANCH: '"development"',
-        API_HOST: '"localhost"',
-        API_PORT: '8080',
-        API_PROTOCOL: '"http"'
+        RELEASE_BRANCH: '"production"',
+        API_HOST: '"go-api.twreporter.org"',
+        API_PORT: '443',
+        API_PROTOCOL: '"https"'
       },
       __CLIENT__: true,
       __DEVELOPMENT__: !isProduction,
