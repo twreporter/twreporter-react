4f6d53a4597017ae55b1c96e5bdca3083e238ed0
nickhsine <nickhsine97753017@gmail.com>
2018-01-16 17:41:37 +0800

    Enable pagination on tag/category listing page

--- src/containers/Category.js
+++ src/containers/Category.js
@@ -1,5 +1,5 @@
 import Helmet from 'react-helmet'
-import More from '../components/More'
+import Pagination from '../components/Pagination'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import SystemError from '../components/SystemError'
@@ -20,10 +20,6 @@ const _  = {
 const { actions, reduxStateFields, utils } = twreporterRedux
 const { fetchListedPosts } = actions
 
-if (process.env.BROWSER) {
-  require('./Category.css')
-}
-
 const MAXRESULT = 10
 const categories = 'categories'
 
@@ -33,59 +29,53 @@ function getListID(paramCategory) {
 }
 
 class Category extends Component {
-  static fetchData({ params, store }) {
-    return store.dispatch(fetchListedPosts(getListID(params.category), categories, MAXRESULT))
-  }
-
-  constructor(props) {
-    super(props)
-    const category = this.props.params.category
-    this.state = {
-      catId: getListID(category)
+  // params are passed from Route component of react-router
+  static fetchData({ params, store, query }) {
+    /* fetch page 1 if page is invalid */
+    let page = parseInt(_.get(query, 'page', 1), 10)
+    if (isNaN(page) || page < 0) {
+      page = 1
     }
-    this.loadMore = this._loadMore.bind(this)
+    return store.dispatch(fetchListedPosts(getListID(params.category), categories, MAXRESULT, page))
   }
 
   componentWillMount() {
-    const { lists, fetchListedPosts } = this.props
+    const { fetchListedPosts, catId } = this.props
+    const page = _.get(this.props, 'page', 1)
 
-    let catId = this.state.catId
-
-    // if fetched before, do nothing
-    if (_.get(lists, [ catId, 'items', 'length' ], 0) > 0) {
-      return
-    }
-
-    fetchListedPosts(catId, categories, MAXRESULT)
-
+    fetchListedPosts(catId, categories, MAXRESULT, page)
   }
 
   componentWillReceiveProps(nextProps) {
-    const { lists, fetchListedPosts, params } = nextProps
-    let catId = getListID(params.category)
+    const { fetchListedPosts, catId } = nextProps
+    const page = _.get(nextProps, 'page', 1)
 
-    // if fetched before, do nothing
-    if (_.get(lists, [ catId, 'items', 'length' ], 0) > 0) {
-      return
-    }
-
-    fetchListedPosts(catId, categories, MAXRESULT)
-  }
-
-  _loadMore() {
-    const { fetchListedPosts, params } = this.props
-    let catId = getListID(params.category)
-    fetchListedPosts(catId, categories, MAXRESULT)
+    fetchListedPosts(catId, categories, MAXRESULT, page)
   }
 
   render() {
-    const { lists, entities, params } = this.props
-    const category = _.get(params, 'category', '')
+    const { lists, entities, catId, category, page, pathname } = this.props
     const postEntities = _.get(entities, reduxStateFields.postsInEntities, {})
-    const catId = getListID(category)
     const error = _.get(lists, [ catId, 'error' ], null)
+
+    // total items will be in that catId
     const total = _.get(lists, [ catId, 'total' ], 0)
-    const posts = utils.denormalizePosts(_.get(lists, [ catId, 'items' ], []), postEntities)
+
+    // pages will be like
+    // {
+    //   1: [0, 9],
+    //   3: [10, 19],
+    // }
+    //
+    // which means the items of page 1 are in items[0] to items[9],
+    // the items of page 3 are in items[10] to item [19]
+    const pages = _.get(lists, [ catId, 'pages' ], {})
+    const startPos = _.get(pages, [ page, 0 ], 0)
+    const endPos = _.get(pages, [ page, 1 ], 0)
+    // denormalize the items of current page
+    const posts = utils.denormalizePosts(_.get(lists, [ catId, 'items' ], []).slice(startPos, endPos + 1), postEntities)
+
+    const totalPages = total / MAXRESULT
     const postsLen = _.get(posts, 'length', 0)
     let isFetching = false
 
@@ -101,12 +91,6 @@ class Category extends Component {
     const catName = categoryString[camelize(category)]
     const title = catName + SITE_NAME.SEPARATOR + SITE_NAME.FULL
     const canonical = `${SITE_META.URL}category/${category}`
-
-    const MoreJSX = total > posts.length ? (
-      <More loadMore={this.loadMore}>
-        <span style={{ color: error ? 'red' : 'white' }}>{error ? '更多文章（請重試）' : '更多文章'}</span>
-      </More>
-    ) : null
 
     return (
       <div>
@@ -132,27 +116,46 @@ class Category extends Component {
           catName={catName}
           isFetching={isFetching}
         />
-        {MoreJSX}
+        <Pagination
+          currentPage={page}
+          totalPages={totalPages}
+          pathname={pathname}
+        />
       </div>
     )
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state, props) {
+  const location = _.get(props, 'location')
+  const params = _.get(props, 'params')
+  const page = parseInt(_.get(location, 'query.page', 1), 10)
+  const category = _.get(params, 'category')
+  const catId = getListID(category)
+  const pathname = _.get(location, 'pathname', `/categories/${category}`)
   return {
     lists: state[reduxStateFields.lists],
-    entities: state[reduxStateFields.entities]
+    entities: state[reduxStateFields.entities],
+    catId,
+    category,
+    page,
+    pathname
   }
 }
 
 Category.defaultProps = {
   entities: {},
-  lists: {}
+  lists: {},
+  catId: ''
 }
 
 Category.propTypes = {
   entities: PropTypes.object,
-  lists: PropTypes.object
+  lists: PropTypes.object,
+  catId: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  page: PropTypes.number.isRequired,
+  pathname: PropTypes.string.isRequired
 }
 
 export { Category }
