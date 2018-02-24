aac1099759b80718d1ea15b9d39bf760333dc0b2
nickhsine <nickhsine97753017@gmail.com>
2018-02-24 18:55:27 +0800

    Move files and rename

--- src/components/article/layout/meta.js
+++ src/components/article/layout/meta.js
@@ -1,12 +1,14 @@
-import { articleLayout as layout } from '../../themes/layout'
-import { HeadingAuthor } from './HeadingAuthor'
-import { PublishDate } from './PublishDate'
-import { screen } from '../../themes/screen'
-import { ShareBt } from './ShareBt'
-import FontChangeButton from '../FontChangeButton'
-import PrintButton from '../shared/PrintButton'
+import FontChangeButton from '../../FontChangeButton'
+import PrintButton from '../../shared/PrintButton'
+import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
+import { HeadingAuthor } from '../HeadingAuthor'
+import { PublishDate } from '../PublishDate'
+import { ShareBt } from '../ShareBt'
+import { articleLayout as layout } from '../../../themes/layout'
+import { getAbsPath } from '../../../utils/url'
+import { screen } from '../../../themes/screen'
 
 const Container = styled.div`
   display: block;
@@ -28,7 +30,8 @@ const Container = styled.div`
 
 class ArticleMeta extends React.Component {
   render() {
-    const { authors, extendByline, publishedDate, appId, url, title, changeFontSize, fontSize } = this.props
+    const { authors, extendByline, publishedDate, appId, title, changeFontSize, fontSize } = this.props
+    const cUrl = getAbsPath(this.context.location.pathname, this.context.location.search)
     return (
       <Container>
         <HeadingAuthor
@@ -42,7 +45,7 @@ class ArticleMeta extends React.Component {
         <div className={'hidden-print'}>
           <ShareBt
             appId={appId}
-            url={url}
+            url={cUrl}
             title={title}
           />
           <PrintButton />
@@ -53,6 +56,10 @@ class ArticleMeta extends React.Component {
       </Container>
     )
   }
+}
+
+ArticleMeta.contextTypes = {
+  location: PropTypes.object
 }
 
 export default ArticleMeta
