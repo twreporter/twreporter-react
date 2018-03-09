d0672907f471bebaa2e882d243bc95376d58cf63
yucj <nakts0123@gmail.com>
2018-03-09 15:08:32 +0800

    Add font constants and web font

---
+++ src/components/about-us/web-font.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const typekitScripts = `
+  (function(d) {
+    var config = {
+      kitId: 'nal4paz',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+`
+
+const WebFont = () => (
+  <script
+    dangerouslySetInnerHTML={{ __html: typekitScripts }}
+  />
+)
+
+export default WebFont
