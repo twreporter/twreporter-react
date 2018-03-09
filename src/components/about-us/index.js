84a7585c4e628243f561b942a749532b72e1d1a0
yucj <nakts0123@gmail.com>
2018-03-09 15:08:32 +0800

    Add font constants and web font

--- src/components/about-us/index.js
+++ src/components/about-us/index.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react'
 import Footer from '@twreporter/react-components/lib/footer'
+import WebFont from './web-font'
 
 export class AboutUs extends PureComponent {
   render() {
     return (
       <React.Fragment>
         <Footer />
+        <WebFont />
       </React.Fragment>
     )
   }
