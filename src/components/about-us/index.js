35abec4f721175637a2c9c38a6c7241cdb6ece7e
Taylor Fang <stu005606@gmail.com>
2018-03-28 10:34:17 +0800

    Add section title of section5

--- src/components/about-us/index.js
+++ src/components/about-us/index.js
@@ -3,6 +3,7 @@ import Opening from './opening'
 import React, { PureComponent } from 'react'
 import Section01 from './section-01'
 import Section04 from './section-04'
+import Section05 from './section-05'
 import WebFont from './web-font'
 
 export class AboutUs extends PureComponent {
@@ -12,6 +13,7 @@ export class AboutUs extends PureComponent {
         <Opening />
         <Section01 />
         <Section04 />
+        <Section05 />
         <Footer />
         <WebFont />
       </React.Fragment>
---
+++ src/components/about-us/section-05/section-title.js
@@ -0,0 +1,113 @@
+import { font } from '../constants/styles'
+import { getMobileSectionTitleStyle } from '../utils/get-section-title-style'
+import { screen } from '../utils/screen'
+import bigNumber from '../../../../static/asset/about-us/title-05.png'
+import mobileTitleImage from '../../../../static/asset/about-us/mobile-title-05.png'
+import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+
+
+const sectionTitleStyle = {
+  containerWidth: {
+    tablet: '571px',
+    desktop: '763px',
+    hd: '926px'
+  },
+  number: {
+    height: {
+      mobile: '281px',
+      tablet: '281px',
+      desktop: '404px',
+      hd: '404px'
+    },
+    width: {
+      tablet: '162px',
+      desktop: '233px',
+      hd: '233px'
+    }
+  }
+}
+
+const containerWidth = sectionTitleStyle.containerWidth
+const numberHeight = sectionTitleStyle.number.height
+const numberWidth = sectionTitleStyle.number.width      
+
+const Container = styled.div`
+  position: relative;
+  background-repeat: no-repeat;
+  background-position: right center;
+  margin: 0 auto;
+  z-index: '5';
+  ${screen.mobile`
+    ${getMobileSectionTitleStyle(mobileTitleImage, '101px')}
+  `}
+  ${screen.tablet`
+    background-image: url(${bigNumber});
+    background-size: ${numberWidth.tablet} ${numberHeight.tablet};
+    width: ${containerWidth.tablet};
+    height: ${numberHeight.tablet};
+  `}
+  ${screen.desktopAbove`
+    background-image: url(${bigNumber});
+    background-size: ${numberWidth.desktop} ${numberHeight.desktop};
+    width: ${containerWidth.desktop};
+    height: ${numberHeight.desktop};
+  `}
+  ${screen.overDesktop`
+    width: ${containerWidth.hd};
+  `}
+`
+
+const TitleWrapper = styled.div`
+  position: relative;
+  ${screen.mobile`
+    display: none;
+  `}
+  ${screen.tablet`
+    top: 93px;
+  `}
+  ${screen.desktop`
+    top: 62px;
+  `}
+  ${screen.overDesktop`
+    top: 62px;
+  `}
+`
+
+const EnglishTitle = styled.div`
+  font-family: ${font.family.english.roboto}, ${font.family.sansSerifFallback};
+  font-weight: ${font.weight.bold};
+  ${screen.tablet`
+    letter-spacing: 17.1px;
+    font-size: 31px;
+  `}
+  ${screen.desktop`
+    letter-spacing: 19.8px;
+    font-size: 36px;
+  `}
+  ${screen.overDesktop`
+    letter-spacing: 19.8px;
+    font-size: 36px;
+  `}
+`
+
+const Title = styled.div`
+  font-family: ${font.family.chinese}, ${font.family.sansSerifFallback};
+  font-weight: ${font.weight.bold};
+  font-size: 24px;
+  letter-spacing: 5.6px;
+  margin-top: 29px;
+`
+
+export default class SectionTitle extends PureComponent {
+  render() {
+    return (
+      <Container>
+        <TitleWrapper>
+          <EnglishTitle>HIGHTLIGHT</EnglishTitle>
+          <Title>大事紀</Title>
+        </TitleWrapper>
+      </Container>
+    )
+  }
+}
