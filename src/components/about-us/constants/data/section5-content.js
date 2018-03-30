b3741d94bb6af2171c99b51450d3eade112e4b84
Taylor Fang <stu005606@gmail.com>
2018-03-30 08:37:23 +0800

    Add data of section5

---
+++ src/components/about-us/constants/data/section5-content.js
@@ -0,0 +1,157 @@
+export const content = [
+  {
+    year: 2018,
+    month: 3,
+    date: 18,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '《報導者》獲第11屆「台灣新聞攝影大賽」7項大獎與「年度最佳攝影記者」殊榮。',
+    link: ''
+  },
+  {
+    year: 2018,
+    month: 1,
+    date: 25,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '《推出《六輕營運20年：科學戰爭下的環境難民》專題報導，並舉辦全台四場面對面座談活動。',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 11,
+    date: 29,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '「造假．剝削．血淚漁場──直擊台灣遠洋漁業真相」獲【2017卓越新聞獎專題新聞獎】調查報導獎',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 10,
+    date: 27,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '5件作品入圍第16屆【卓越新聞獎】，包括「 好死好難 ？」、「誰讓台灣公僕變庸才？」、「他們逃到德國後 」、「卞中佩專欄：從川普現象看時代的轉變 」、「造假．剝削血淚漁場」',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 7,
+    date: 3,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '專訪行政院長林全，並推出【體檢前瞻】專題報導',
+    link: 'https://www.twreporter.org/topics/foresight-infrastructure-plan'
+  },
+  {
+    year: 2017,
+    month: 6,
+    date: 15,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '專訪行政院長林全，並推出【體檢前瞻】專題報導',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 5,
+    date: 16,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '舉辦首場《血淚漁場：跨國直擊台灣遠洋漁業真相》新書分享會',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 4,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '出版第一本調查報導專書《血淚漁場：跨國直擊台灣遠洋漁業真相》',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 4,
+    date: 10,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '《造假．剝削．血淚漁場──直擊台灣遠洋漁業真相》受「國際調查記者聯盟」（GIJN）重視，並採訪《報導者》與《Tempo》團隊',
+    link: 'https://gijn.org/2017/04/10/indonesias-tempo-leads-asia-into-cross-border-collaborations/'
+  },
+  {
+    year: 2017,
+    month: 3,
+    date: 25,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '於高雄舉辦「報導者Ｘ大港開唱：人生。扭一下」活動',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 2,
+    date: 25,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '舉辦為期四個月「報導者攝影工作坊II：在地影像扎根計畫」',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 2,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '授權出版《透明的小孩─無國籍移工兒童的故事》繪本',
+    link: ''
+  },
+  {
+    year: 2017,
+    month: 1,
+    date: 14,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '首度舉辦「調查報導工作坊」及「攝影工作坊」',
+    link: ''
+  },
+  {
+    year: 2016,
+    month: 12,
+    date: 18,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '發表年度調查報導《造假．剝削．血淚漁場──直擊台灣遠洋漁業真相》',
+    link: 'https://www.twreporter.org/topics/far-sea-fishing-investigative-report'
+  },
+  {
+    year: 2016,
+    month: 10,
+    date: 25,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '《報導者》製作台灣第一個新聞遊戲「急診人生」，入圍【2016卓越新聞獎專題新聞獎】',
+    link: ''
+  },
+  {
+    year: 2016,
+    month: 9,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '巡迴北中南東，舉辦5場「誰是報導者」系列講座',
+    link: ''
+  },
+  {
+    year: 2016,
+    month: 8,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '啟動線上定期定額贊助計畫、完成第一支報導者形象影片',
+    link: ''
+  },
+  {
+    year: 2016,
+    month: 6,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '舉辦首場「屋頂上的讀者會」，邀請各領域讀者與報導者們面對面對談',
+    link: ''
+  },
+  {
+    year: 2015,
+    month: 12,
+    date: 16,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '《報導者》網站正式上線',
+    link: ''
+  },
+  {
+    year: 2015,
+    month: 9,
+    date: 1,
+    image: 'https://storage.googleapis.com/twreporter-multimedia/images/about-us/indonesia.jpg',
+    text: '成立「財團法人報導者文化基金會」',
+    link: ''
+  }
+]
