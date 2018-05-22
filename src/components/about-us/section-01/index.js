c2f7952d9546cd8d910a4e8046e1534b04eff64e
Taylor Fang <stu005606@gmail.com>
2018-05-22 12:14:49 +0800

    Adds section01 of about-us page 2.0 (RWD)

--- src/components/about-us/section-01/index.js
+++ src/components/about-us/section-01/index.js
@@ -1,47 +1,55 @@
 import { marginBetweenSections } from '../constants/styles'
 import { screen } from '../utils/screen'
+import LottieAnim from './lottie-animation.js'
 import React, { PureComponent } from 'react'
 import styled from 'styled-components'
-import LottieAnim from './lottie-animation.js'
 import titleImg from '../../../../static/asset/about-us/title-section1.png'
-
-const containerWidth = {
-  mobile: '100%',
-  tablet: '719px',
-  desktop: '1024px',
-  overDesktop: '1440px'
-}
+import titleImgMob from '../../../../static/asset/about-us/title-section1-mob.png'
 
 const Container = styled.div`
   position: relative;
   margin: 0 auto;
   background-color: white;
-  overflow: hidden;  
+  overflow: hidden;
+  border: solid 8px #c71b0c;
+  height: 100vh;  
   ${screen.desktopAbove`
-    width: ${containerWidth.desktop};
-    margin: ${marginBetweenSections.desktop} auto;
+    margin: ${marginBetweenSections.desktop} 0;
   `}
   ${screen.overDesktop`
-    width: ${containerWidth.overDesktop};
-    margin: ${marginBetweenSections.overDesktop} auto;    
+    margin: ${marginBetweenSections.overDesktop} 0;    
   `}
   ${screen.tablet`
-    width: ${containerWidth.tablet};
-    margin: ${marginBetweenSections.tablet} auto;    
+    margin: ${marginBetweenSections.tablet} 0;    
   `}  
   ${screen.mobile`
-    margin: ${marginBetweenSections.mobile} auto;    
+    margin: ${marginBetweenSections.mobile} 0;    
   `}    
 `
 
-const SectionBlock = styled.div`
+const SectionWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
   display: block;
-  width: 100%;
-  height: 820px;
-  border: solid 8px #c71b0c;
-  padding: 156px 179px 117px 131px;
+  ${screen.desktop`
+    width: 1024px;
+    height: 547px;
+    padding: 0 179px 0 130px;
+  `}  
   ${screen.overDesktop`
-    padding: 150px 262px 84px 172px;
+    width: 1440px;
+    height: 586px;
+    padding: 0 262px 0 170px;
+  `}
+  ${screen.tablet`
+    width: 432px;
+    height: 849px;
+  `}
+  ${screen.mobile`
+    width: 291px;
+    height: 571px;
   `}
 `
 
@@ -49,43 +57,130 @@ const Title = styled.div`
   background-image: url(${titleImg});
   background-repeat: no-repeat;
   background-size: contain;
-  width: 283px;
-  height: calc(100% - 111px);
+  height: 60%;
   float: left;
   border-bottom: solid 27px #a67a44;
-  width: 194px;
+  ${screen.desktop`
+    width: 193.7px;
+    height: 330.3px;
+  `}
   ${screen.overDesktop`
-    width: 283px;
+    width: 276px;
+    height: 475px;
+  `}
+  ${screen.tabletBelow`
+    background-image: url(${titleImgMob});
+    background-position: center top;
+    float: none;
+    margin: 0 auto;
+  `}
+  ${screen.tablet`
+    width: 134px;
+    height: 300px;
+    border-bottom: solid 18.9px #a67a44;
+  `}
+  ${screen.mobile`
+    width: 84px;
+    height: 195px;
+    border-bottom: solid 18px #a67a44;
   `}
 `
 
 const Introduction = styled.div`
+  margin-top: 65px;
   p{
+    display: inline;
     font-size: 18px;
     font-weight: 500;
-    text-align: left;
     line-height: 1.89;
+    text-align: left;
   }
-  margin-top: 65px;
+  ul{
+    text-align: left;
+    list-style: none;
+    margin: 0;
+    padding-left: 1em;
+  }
+  ul>li:before {
+    display: inline-block;
+    content: "-";
+    margin-left: -1em;
+  }
+  ul>p:first-child{
+    margin-left: -1em;
+  }
+  ${screen.desktopAbove`
+    p{
+      background-color: #f6f6f6;
+      padding: 5px;
+    }  
+    ul>li:before {
+      background-color: #f6f6f6;
+      padding: 5px 0 5px 10px;
+    }
+  `}
+  ${screen.tabletBelow`
+    ul{
+      background: #f6f6f6;
+    }
+    ul>li:before {
+      width: 0.5em;    
+    } 
+  `}
+  ${screen.tablet`
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.67;
+    text-align: left;
+  `}
+  ${screen.mobile`
+    margin: 0 20px;
+    p{
+      font-size: 16px;
+      line-height: 1.44;
+      letter-spacing: 0.5px;
+      text-align: left;
+    }
+  `}  
 `
 
 const Content = styled.div`
-  width: 384px;
-  ${screen.overDesktop`
-    width: 400px;
-  `}
   height: 100%;
   float: right;
-  /* border: solid 1px green; */
   text-align: center;
   h2{
     display: inline-block;
     margin-top: 0;
-    margin-bottom: 107px;
+    margin-bottom: 90px;
     font-size: 28px;
     font-weight: bold;
     letter-spacing: 6.6px;
   }
+  ${screen.desktopAbove`
+    width: 384px;
+  `}
+  ${screen.tabletBelow`
+    width: 100%;
+    float: none;
+  `}
+  ${screen.tablet`
+    height: calc(100% - 134px);
+    h2{
+      margin-top: 80px;
+      margin-bottom: 45px;
+      font-size: 20px;
+      letter-spacing: 4.7px;
+    }  
+  `}  
+  ${screen.mobile`
+    height: calc(100% - 195px);
+    margin-top: 50px;
+    h2{
+      margin: 0;
+      font-size: 20px;
+      letter-spacing: 4.7px;
+    }  
+  `}  
 `
 
 const Caption = styled.div`
@@ -131,7 +226,7 @@ export class Section1 extends PureComponent {
     })
     return (
       <Container>
-        <SectionBlock>
+        <SectionWrapper>
           <Title />
           <Content>
             {Captions}
@@ -139,13 +234,15 @@ export class Section1 extends PureComponent {
               animDidUpdate={this._animUpdated}
             />
             <Introduction>
-              <p>屬於社會的《報導者》</p>
-              <p>- CC授權（ 姓名標示／非商業性／禁止改作</p>
-              <p>- OPEN SOURCE開放原始碼</p>
-              <p>- 全民政策追蹤： 蔡英文勞動政策追蹤平台</p>
+              <ul>
+                <p>屬於社會的《報導者》</p>
+                <li><p>CC授權（ 姓名標示／非商業性／禁止改作</p></li>
+                <li><p>OPEN SOURCE開放原始碼</p></li>
+                <li><p>全民政策追蹤： 蔡英文勞動政策追蹤平台</p></li>
+              </ul>
             </Introduction>    
-          </Content>  
-        </SectionBlock>
+          </Content>
+        </SectionWrapper>
       </Container>
     )
   }
