8f07de6ebacf4c8a01946eb990cd8bef15b200b5
Taylor Fang <stu005606@gmail.com>
2018-05-29 11:28:44 +0800

    Split lottie library and animation files into chunks which would be dynamically imported

--- src/components/about-us/section-01/lottie-animation.js
+++ src/components/about-us/section-01/lottie-animation.js
@@ -1,12 +1,14 @@
+// polyfill webpack require.ensure for node environment
+if (typeof require.ensure !== 'function') require.ensure = (d, c) => c(require)
+
 import { screen } from '../utils/screen'
-import animationData1 from '../../../../static/asset/about-us/animation/section1-1.json'
-import animationData2 from '../../../../static/asset/about-us/animation/section1-2.json'
-import animationData3 from '../../../../static/asset/about-us/animation/section1-3.json'
-import Lottie from 'react-lottie'
+import Loadable from 'react-loadable'
 import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 
-const LottieContainer = styled.div`
+const animationLength = 3
+
+const Container = styled.div`
   width: 100%;
   height: 38.39%;
   ${screen.tablet`
@@ -19,28 +21,64 @@ const LottieContainer = styled.div`
   `}
 `
 
-export class LottieAnim extends PureComponent {
+export default class LottieAnim extends PureComponent {
   constructor(props) {
     super(props)
-    this.dataset = [ animationData1, animationData2, animationData3 ]
     this.currentData = 0
     this.state = {
-      animationData: this.dataset[this.currentData]
+      dataIndex: 0
     }
   }
-  _animationUpdate() {
-    let dataIndex = ++this.currentData % this.dataset.length
-    this.setState({ animationData: this.dataset[dataIndex] })
+  _animationUpdate = () => {
+    let dataIndex = ++this.currentData % animationLength
+    this.setState({ dataIndex: dataIndex })
     this.props.animDidUpdate(dataIndex)
+  }
+  _getLottieComponent = () => {
+    const modulePlaceHolder = null
+    const LoadableComponent = Loadable.Map({
+      loader: { 
+        Lottie: () => import(
+          /* webpackChunkName: "lottie" */
+          '../utils/lottie'
+        ),
+        anim1: () => import(
+          /* webpackChunkName: "aboutus-section1-anim1" */
+          '../../../../static/asset/about-us/animation/section1-1.json'
+        ),
+        anim2: () => import(
+          /* webpackChunkName: "aboutus-section1-anim2" */
+          '../../../../static/asset/about-us/animation/section1-2.json'
+        ),
+        anim3: () => import(
+          /* webpackChunkName: "aboutus-section1-anim3" */
+          '../../../../static/asset/about-us/animation/section1-3.json'
+        )        
+      },
+      render(loaded, props) {
+        if (loaded.Lottie) {
+          const Lottie = loaded.Lottie.default
+          const { anim1, anim2, anim3 } = loaded
+          this.animations = [ anim1, anim2, anim3 ]
+          props.options.animationData = this.animations[props.dataIndex]
+          return <Lottie {...props} />      
+        }
+        return modulePlaceHolder
+      },
+      loading() {
+        return modulePlaceHolder
+      }
+    })
+    return LoadableComponent
   }
   render() {
     let options = {
       loop: true,
-      autoplay: true,
-      animationData: this.state.animationData
+      autoplay: true
     }
+    const Lottie = this._getLottieComponent()
     return (
-      <LottieContainer>
+      <Container>
         <Lottie
           options={options}
           isStopped={false}
@@ -51,11 +89,10 @@ export class LottieAnim extends PureComponent {
               callback: () => this._animationUpdate()
             } ]
           }
-          />
-      </LottieContainer>
+          dataIndex={this.state.dataIndex}
+        />
+      </Container>
     )
   }
 }
-
-export default LottieAnim
 
