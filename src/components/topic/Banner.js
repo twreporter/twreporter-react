7e7bcd808e05e3a3cc5a38384e27dd69cffc271f
yucj <nakts0123@gmail.com>
2018-07-20 14:15:09 +0800

    Change Banner to class component

--- src/components/topic/Banner.js
+++ src/components/topic/Banner.js
@@ -1,4 +1,6 @@
-'use strict'
+import { addStylesToPropsDecorator } from '../shared/ComponentDecorators'
+import { date2yyyymmdd } from '../../utils/date'
+import { TOPIC_LAST_UPDATED } from '../../constants/index'
 import ArrowDownIcon from '../../../static/asset/arrow-down.svg'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -6,49 +8,54 @@ import smoothScroll from 'smoothscroll'
 import stylesBottom from './BannerBottom.scss'
 import stylesBottomLeft from './BannerBottomLeft.scss'
 import stylesCenter from './BannerCenter.scss'
-import { TOPIC_LAST_UPDATED } from '../../constants/index'
-import { addStylesToPropsDecorator } from '../shared/ComponentDecorators'
-import { date2yyyymmdd } from '../../utils/date'
 
-/**
- * Add tail space when head is a fullwidth bracket for visually centering
- *
- * @param {string} text
- * @returns {string}
- */
-function addTailSpaceIfHeadIsFullwidthBracket(text) {
-  if (typeof text === 'string') {
-    const leftBrackets = [ '（', '【', '〔', '《', '〈', '｛', '『', '「' ]
-    for (let i=0, length=leftBrackets.length; i<length; i++) {
-      if (typeof text === 'string' && text.startsWith(leftBrackets[i])) {
-        return (text + '  ')
-      }
-    }
+class Banner extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.handleScroll = this._handleScroll.bind(this)
   }
-  return text
-}
-
-const Banner = (props) => {
-  const { headline, title, subtitle, publishedDate, styles } = props
-  const _handleScroll = function (e) {
+  _handleScroll(e) {
     e.preventDefault()
     if (typeof window !== 'object') return
     return smoothScroll(window.innerHeight)
   }
-  return (
-    <div className={styles['banner-container']}>
-      <div className={styles['infos-flex-container']} >
-        {!headline ? null : <div className={styles['headline']} >{addTailSpaceIfHeadIsFullwidthBracket(headline)}</div>}
-        <h1 className={styles['title']} >{title}</h1>
-        {!subtitle ? null : <h2 className={styles['subtitle']} >{subtitle}</h2>}
-        <div className={styles['dash']} ></div>
-        {!publishedDate ? null : <div className={styles['published-date']} >{date2yyyymmdd(publishedDate, '.')+' '+TOPIC_LAST_UPDATED}</div>}
+
+  /**
+   * Add tail space when head is a fullwidth bracket for visually centering
+   *
+   * @param {string} text
+   * @returns {string}
+   * @memberof Banner
+   */
+  _addTailSpaceIfHeadIsFullwidthBracket(text) {
+    if (typeof text === 'string') {
+      const leftBrackets = [ '（', '【', '〔', '《', '〈', '｛', '『', '「' ]
+      for (let i=0, length=leftBrackets.length; i<length; i++) {
+        if (typeof text === 'string' && text.startsWith(leftBrackets[i])) {
+          return (text + '  ')
+        }
+      }
+    }
+    return text
+  }
+
+  render() {
+    const { headline, title, subtitle, publishedDate, styles } = this.props
+    return (
+      <div className={styles['banner-container']}>
+        <div className={styles['infos-flex-container']} >
+          {!headline ? null : <div className={styles['headline']} >{this._addTailSpaceIfHeadIsFullwidthBracket(headline)}</div>}
+          <h1 className={styles['title']} >{title}</h1>
+          {!subtitle ? null : <h2 className={styles['subtitle']} >{subtitle}</h2>}
+          <div className={styles['dash']} ></div>
+          {!publishedDate ? null : <div className={styles['published-date']} >{date2yyyymmdd(publishedDate, '.')+' '+TOPIC_LAST_UPDATED}</div>}
+        </div>
+        <div className={styles['arrow-down-icon-wrapper']} onClick={this.handleScroll} >
+          <ArrowDownIcon />
+        </div>
       </div>
-      <div className={styles['arrow-down-icon-wrapper']} onClick={_handleScroll} >
-        <ArrowDownIcon />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 Banner.propTypes = {
