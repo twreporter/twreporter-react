4ca9642dc61b8810b39d47f38fbd9b0986925c7e
yucj <nakts0123@gmail.com>
2019-06-18 21:02:54 +0800

    Set api origins to redux store

--- src/express/middlewares/init-redux-store.js
+++ src/express/middlewares/init-redux-store.js
@@ -1,6 +1,10 @@
 /* global __DEVELOPMENT__ */
-import set from 'lodash/set'
+import requestOrigins from '../../constants/request-origins'
 import twreporterRedux from '@twreporter/redux'
+// lodash
+import set from 'lodash/set'
+
+const stateFields = twreporterRedux.reduxStateFields
 
 const _ = {
   set
@@ -10,12 +14,17 @@ const _ = {
  *  This middleware inits redux store,
  *  and set it as `req[namespace].reduxStore`
  *  @param {string} namespace - namespace is used in `req` object to avoid from overwriting the existed field
+ *  @param {Object} options
+ *  @param {string} options.releaseBranch - release branch, it could be 'master', 'test', 'staging' or 'release'
  */
-function initReduxStoreMiddleware(namespace) {
+function initReduxStoreMiddleware(namespace, options) {
   return async function middleware(req, res, next) {
     try {
       const cookieList = req.get('cookie')
-      const reduxStore = await twreporterRedux.createStore({}, cookieList, __DEVELOPMENT__)
+      const initState = {
+        [stateFields.origins]: requestOrigins.fromServer[options.releaseBranch]
+      }
+      const reduxStore = await twreporterRedux.createStore(initState, cookieList, __DEVELOPMENT__)
       _.set(req, [ namespace, 'reduxStore' ], reduxStore)
       next()
     } catch(err) {
