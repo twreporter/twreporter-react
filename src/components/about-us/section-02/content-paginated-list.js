f7f34a8ddad3b543d681184e05ed4b5bfdf69d2c
yucj <nakts0123@gmail.com>
2019-06-25 14:56:14 +0800

    Move utils to @twreporter/core

--- src/components/about-us/section-02/content-paginated-list.js
+++ src/components/about-us/section-02/content-paginated-list.js
@@ -1,6 +1,6 @@
 import { colors } from '../../../themes/common-variables'
 import { gray, numbersInfullPage } from './utils'
-import { replaceStorageUrlPrefix } from '@twreporter/react-components/lib/shared/utils'
+import { replaceStorageUrlPrefix } from '@twreporter/core/lib/utils/storage-url-processor'
 import { screen } from '../utils/screen'
 import { storageUrlPrefix } from '../utils/config'
 import Arrows from './arrows'
