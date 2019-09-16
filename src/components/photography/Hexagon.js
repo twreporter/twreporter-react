eff22d01b4d14d1fcbf82271665f6c13843e9512
yucj <nakts0123@gmail.com>
2019-09-16 14:40:19 +0800

    Add assets of photography

--- src/components/photography/Hexagon.js
+++ src/components/photography/Hexagon.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Hexagon = styled.div`
   width: 2.6em;
-	background-image: url('data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22330%22%20height%3D%22330%22%3E%3Cpolygon%20fill%3D%22%23fff%22%20stroke%3D%22%23000%22%20stroke-width%3D%2215%22%20stroke-linejoin%3D%22round%22%20points%3D%22315%2C165%20240%2C295%2090%2C295%2015%2C165%2090%2C35%20240%2C35%22%3E%3C%2Fpolygon%3E%3C%2Fsvg%3E');
+	background-image: url('/asset/photography/polygon.svg');
   background-size: 100%;
   background-repeat: repeat-y;
   text-align: center;
