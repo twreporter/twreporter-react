172143099c1cc462935ccb38640e2c5179d42980
Taylor Fang <stu005606@gmail.com>
2019-10-31 15:26:28 +0800

    Address review comments
    - Set `characterData` to be true in the options param of
      `MutationObserver.observe()`
    - Make `observer` and `asyncTimeId` be local variables of
      `hashLinkScroll` function
    - Rename function `getElementAndScroll` to `scrollToElement` and remove
      its params `observer` and `asyncTimeId`

--- src/utils/hash-link-scroll.js
+++ src/utils/hash-link-scroll.js
@@ -1,7 +1,9 @@
-let observer = null
-let asyncTimerId = null
-
-function reset() {
+/*
+ * @param {Object} observer - A MutationObserver object 
+ * @param {number} asyncTimerId - A positive integer value which identifies the timer created by the call to setTimeout()
+ * @returns {void}
+ */
+function reset(observer, asyncTimerId) {
   if (observer) {
     observer.disconnect()
   }
@@ -10,11 +12,14 @@ function reset() {
   }
 }
 
-function getElementAndScroll(id) {
+/*
+ * @param {string} id - element id 
+ * @returns {boolean} - represents whether document can get element by id 
+ */
+function scrollToElement(id) {
   const element = document.getElementById(id)
   if (element) {
     element.scrollIntoView()
-    reset()
     return true
   }
   return false
@@ -28,28 +33,32 @@ function hashLinkScroll() {
     if (hash !== '') {
       // Push onto callback queue so it runs after the DOM is updated
       setTimeout(() => {
+        let observer = null
+        let asyncTimerId = null
         const id = hash.replace('#', '')
-        // since id will be encoded by browser,
-        // decode it back as a string
-        const decodedID = decodeURIComponent(id)
+        // Since id will be encoded by browser, decode it back as a string
+        const decodedId = decodeURIComponent(id)
         // Create a `MutationObserver` instance to watch for changes being made to the DOM tree.
         // Since `document.getElementById(id)` returns null when navigating from a different page 
         // without element of id, for example: When navigating from an article page to homepage
         // with hash `#categories` in url, `document.getElementById('categories')` returns null.
         // In such case, the `MutationObserver` instance keeps watching and will call it's callback 
         // when the DOM tree changes, page scrolls to the desired element as long as it can be gotten by id.
-        if (!getElementAndScroll(decodedID)) {
-          if (!observer) {
-            observer = new MutationObserver(() => { getElementAndScroll(decodedID) })
-          }
+        if (!scrollToElement(decodedId)) {
+          observer = new MutationObserver(() => {
+            if (scrollToElement(decodedId)) {
+              reset(observer, asyncTimerId)
+            }
+          })
           observer.observe(document, {
             attributes: true,
             childList: true,
+            characterData: true,
             subtree: true
           })
           // if the element doesn't show up in 10 seconds, stop checking
           asyncTimerId = setTimeout(() => {
-            reset()
+            reset(observer, asyncTimerId)
           }, 10000)
         }
       }, 0)
