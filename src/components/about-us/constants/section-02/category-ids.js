6a6f6019e292df2615aecc87b5f18cee88125024
Taylor Fang <stu005606@gmail.com>
2020-04-15 16:12:58 +0800

    refactor(about-us): update constants
    This patch update constants about members and departments

--- src/components/about-us/constants/section-02/category-ids.js
+++ src/components/about-us/constants/section-02/category-ids.js
@@ -1,8 +1,11 @@
 const categoryIds = {
   fundation: 'fundation',
   editor: 'editor',
+  socialMediaManager: 'socialMediaManager',
   photojournalist: 'photojournalist',
-  digital: 'digital'
+  marketing: 'marketing',
+  engineer: 'engineer',
+  designer: 'designer',
 }
 
 export default categoryIds
