e2c8da1a7910ec0b607661d743713af2e9e4cb20
nickhsine <nickhsine97753017@gmail.com>
2020-07-07 11:33:07 +0800

    chore: add src/mock-data/v2/ for v2 endpoints
    - add src/mock-data/v2/index-page.js
    - add src/mock-data/v2/posts.js
    - add src/mock-data/v2/topics.js
    - add src/mock-data/v2/posts.json
    - add src/mock-data/v2/topics.json
    
    `v2/posts.json` and `v2/topics.json` are migrated from
    `fullposts.json`.
    
    `v2/index-page`, `v2/topics.js` and `v2/posts`
    are used to mock the v2 go-api response.

---
+++ src/mock-data/v2/posts.js
@@ -0,0 +1,118 @@
+import posts from './posts.json'
+import { cloneMetaOfPost } from './utils'
+
+// lodash
+import forEach from 'lodash/forEach'
+
+const _ = {
+  forEach,
+}
+
+const mocks = {
+  posts,
+}
+
+/**
+ *  This function seeks the posts with certain ids
+ *
+ *  @param {Object[]} mockPosts - array of mocked posts
+ *  @param {string[]} ids - array of ids
+ *  @return {Object[]} - matched posts
+ */
+function seekPostsByIds(mockPosts, ids) {
+  const rtn = []
+
+  _.forEach(ids, id => {
+    _.forEach(mockPosts, post => {
+      if (post.id === id) {
+        rtn.push(post)
+      }
+    })
+  })
+
+  return rtn
+}
+
+/**
+ *  This function seeks the posts with certain tags or categories.
+ *
+ *  @param {Object[]} mockPosts - array of mocked posts
+ *  @param {string[]} ids - array of ids
+ *  @param {string} listType - category_id or tag_id
+ *  @return {Object[]} - matched posts
+ */
+function seekPostsByListIds(mockPosts, ids, listType) {
+  const rtn = []
+
+  _.forEach(ids, id => {
+    _.forEach(mockPosts, post => {
+      _.forEach(post[listType], list => {
+        if (list.id === id) {
+          rtn.push(post)
+        }
+      })
+    })
+  })
+
+  return rtn
+}
+
+export function mockAPostResponse(slug) {
+}
+
+/**
+ *  This function mocks the response of go-api `/v2/posts` endpoint.
+ *
+ *  @param {number} limit - how many posts in the response
+ *  @param {number} offset - how many posts to skip
+ *  @param {string} id - post id
+ *  @param {string} category_id - category id
+ *  @param {string} tag_id - tag id
+ *  @param {Object} mocked api response
+ */
+export function mockPostsResponse(limit=10, offset=0, id, category_id, tag_id) {
+  let posts = []
+
+  if (id) {
+    let ids = id
+    if (typeof id === 'string') {
+      ids = [id]
+    }
+    posts = posts.concat(seekPostsByIds(mocks.posts, ids))
+  }
+
+  if (category_id) {
+    let ids = category_id
+    if (typeof category_id === 'string') {
+      ids = [category_id]
+    }
+
+    posts = posts.concat(seekPostsByListIds(mocks.posts, ids, 'categories'))
+  }
+
+  if (tag_id) {
+    let ids = tag_id
+    if (typeof tag_id === 'string') {
+      ids = [tag_id]
+    }
+
+    posts = posts.concat(seekPostsByListIds(mocks.posts, ids, 'tags'))
+  }
+
+  return {
+    status: 'success',
+    data: {
+      meta: {
+        limit,
+        offset,
+        total: posts.length,
+      },
+      records: posts.slice(offset, offset + limit).map(cloneMetaOfPost)
+    },
+  }
+}
+
+export default {
+  mockAPostResponse,
+  mockPostsResponse,
+}
