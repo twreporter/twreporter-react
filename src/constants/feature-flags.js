6ae4b6ca20d2d03e229d9576e76333726d69059d
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2022-03-14 14:49:53 +0800

    Jason/revert print header (#2067)
    * fix: revert header media print
    * fix: add missing constant

---
+++ src/constants/feature-flags.js
@@ -0,0 +1,6 @@
+// TODO: delete this flag when PR #2065(https://github.com/twreporter/twreporter-react/pull/2065) is merged
+const disableHeaderPrint = true
+
+export default {
+  disableHeaderPrint,
+}
