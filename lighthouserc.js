150a7d824f944af5a57cd26a3dde4ab85931705c
Tai-Jiun Fang <taylorfang@twreporter.org>
2022-08-18 12:44:52 +0800

    refactor: add article page as the target in lighthouse measurement
    This page adds article page url link as the other target for
    collecting metrics in lighthouse measurement.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,10 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 5,
-      url: ['http://localhost:3000/'],
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/a/russian-invasion-of-ukraine-2022-kid-frequently-asked-questions',
+      ],
       startServerCommand: 'make dev',
     },
     upload: {
