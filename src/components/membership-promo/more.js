bba82ffbf14b876d6e1ed6f57f05a586f437ba1d
Aylie Chou <aylie_chou@twreporter.org>
2023-07-27 11:49:13 +0800

    fix: close model when clicking more button

--- src/components/membership-promo/more.js
+++ src/components/membership-promo/more.js
@@ -1,35 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+// contexts
+import { PromoContext } from '../../contexts'
 // @twreporter
 import requestOrigins from '@twreporter/core/lib/constants/request-origins'
 import Link from '@twreporter/react-components/lib/customized-link'
 import { PillButton } from '@twreporter/react-components/lib/button'
-import {
-  BRANCH,
-  BRANCH_PROP_TYPES,
-} from '@twreporter/core/lib/constants/release-branch'
 
 const StretchPillButton = styled(PillButton)`
   width: auto;
   display: flex;
   justify-content: center;
 `
-const MoreButton = ({ releaseBranch = BRANCH.master, ...props }) => (
-  <Link
-    to={`${requestOrigins.forClientSideRendering[releaseBranch].support}#plan`}
-    {...props}
-  >
-    <StretchPillButton
-      type={PillButton.Type.PRIMARY}
-      size={PillButton.Size.S}
-      style={PillButton.Style.BRAND}
-      theme={PillButton.THEME.normal}
-      text="瞭解更多"
-    />
-  </Link>
-)
-MoreButton.propTypes = {
-  releaseBranch: BRANCH_PROP_TYPES,
+const MoreButton = ({ ...props }) => {
+  const { closePromo, releaseBranch } = useContext(PromoContext)
+
+  return (
+    <Link
+      to={`${requestOrigins.forClientSideRendering[releaseBranch].support}#plan`}
+      isExternal={true}
+      target="_blank"
+      onClick={closePromo}
+      {...props}
+    >
+      <StretchPillButton
+        type={PillButton.Type.PRIMARY}
+        size={PillButton.Size.S}
+        style={PillButton.Style.BRAND}
+        theme={PillButton.THEME.normal}
+        text="瞭解更多"
+      />
+    </Link>
+  )
 }
 
 export default MoreButton
