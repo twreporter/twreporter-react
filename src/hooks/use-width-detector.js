9486f72801467baec8679b3773c2422f22ca8cfc
Lucien <liruchen0302@gmail.com>
2024-01-31 10:02:03 +0800

    feat: add width detector hook

---
+++ src/hooks/use-width-detector.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+const useWidthDetector = targetWidth => {
+  const [width, setWidth] = useState(window.innerWidth)
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange)
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange)
+    }
+  }, [])
+
+  return width <= targetWidth
+}
+
+export default useWidthDetector
