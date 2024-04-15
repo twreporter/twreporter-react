d18a5ef4cbabd9d87113a2b8dfcac900a42ba0b4
Lucien <liruchen0302@gmail.com>
2024-04-15 11:33:36 +0800

    feat: add loading mask and fake data

--- src/components/my-reading/reviewing-article-section.js
+++ src/components/my-reading/reviewing-article-section.js
@@ -6,12 +6,16 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { Title2 } from '@twreporter/react-components/lib/title-bar'
 import Divider from '@twreporter/react-components/lib/divider'
 import { TextButton } from '@twreporter/react-components/lib/button'
-import { colorGrayscale } from '@twreporter/core/lib/constants/color'
+import {
+  colorBrand,
+  colorGrayscale,
+} from '@twreporter/core/lib/constants/color'
 import { Arrow } from '@twreporter/react-components/lib/icon'
 import {
   TabletAndAbove,
   MobileOnly,
 } from '@twreporter/react-components/lib/rwd'
+import FetchingWrapper from '@twreporter/react-components/lib/is-fetching-wrapper'
 
 // components
 import EmptyBox from './empty-box'
@@ -54,11 +58,20 @@ const MoreButton = styled(TextButton)`
   color: ${colorGrayscale.gray800};
 `
 
+const Loading = styled.div``
+const LoadingMask = FetchingWrapper(Loading)
+
+const FakeDataButton = styled.button`
+  margin-left: 2px;
+  ${props => (props.isActive ? `color: ${colorBrand.heavy};` : ``)}
+`
+
 const ReviewingArticleSection = () => {
   const [totalReviewingArticle, setTotalReviewingArticle] = useState(0)
   const [showMore, setShowMore] = useState(false)
   const [reviewingArticles, setReviewingArticles] = useState([])
   const [showingReviewingArticle, setShowingReviewingArticle] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const generateFakeData = length => {
     return Array(length).fill(fakeArticle)
@@ -69,56 +82,102 @@ const ReviewingArticleSection = () => {
     setShowingReviewingArticle(reviewingArticles)
   }
 
+  const testDataButton = () => {
+    const handleClick = e => {
+      setTotalReviewingArticle(Number(e.target.value || 0))
+    }
+    return (
+      <div>
+        <FakeDataButton
+          isActive={totalReviewingArticle === 0}
+          value={0}
+          onClick={handleClick}
+        >
+          0
+        </FakeDataButton>
+        <FakeDataButton
+          isActive={totalReviewingArticle === 2}
+          value={2}
+          onClick={handleClick}
+        >
+          2
+        </FakeDataButton>
+        <FakeDataButton
+          isActive={totalReviewingArticle === 8}
+          value={8}
+          onClick={handleClick}
+        >
+          8
+        </FakeDataButton>
+      </div>
+    )
+  }
+
   useEffect(() => {
-    const fakeData = generateFakeData(8)
+    setIsLoading(true)
+    const fakeData = generateFakeData(totalReviewingArticle)
     setReviewingArticles(fakeData)
-    setShowingReviewingArticle(fakeData.slice(0, 3))
-    setTotalReviewingArticle(fakeData.length)
-    setShowMore(fakeData.length > 3)
-  }, [])
+    if (fakeData.length > 3) {
+      setShowingReviewingArticle(fakeData.slice(0, 3))
+      setShowMore(true)
+    } else {
+      setShowingReviewingArticle(fakeData)
+    }
+    setTimeout(() => setIsLoading(false), 1000)
+  }, [totalReviewingArticle])
 
   return (
     <ReviewingArticleContainer>
-      <Title2 title="報導回顧" subtitle="和報導者編輯台一起，與議題持續對話" />
-      {totalReviewingArticle === 0 && (
-        <EmptyBox type={EmptyBox.Type.ReviewingArticle} />
-      )}
-      {showingReviewingArticle.length > 0 && (
-        <>
-          <TabletAndAbove>
-            <ReviewingArticleList>
-              {showingReviewingArticle.map((article, idx) => {
-                return (
-                  <div key={idx}>
-                    <ReviewingCard {...article} />
-                    <DividerContainer>
-                      <Divider />
-                    </DividerContainer>
-                  </div>
-                )
-              })}
-              {showMore && (
-                <MoreButton
-                  text="展開更多"
-                  rightIconComponent={<Arrow direction="down" />}
-                  onClick={handleShowMoreClick}
-                />
-              )}
-            </ReviewingArticleList>
-          </TabletAndAbove>
-          <MobileOnly>
-            <ReviewingArticleList>
-              <Swiper slidesPerView={'auto'} spaceBetween={24} freeMode={true}>
-                {reviewingArticles.map((article, idx) => (
-                  <SwiperSlide key={idx}>
-                    <ReviewingCard {...article} />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </ReviewingArticleList>
-          </MobileOnly>
-        </>
-      )}
+      <Title2
+        title="報導回顧"
+        subtitle="和報導者編輯台一起，與議題持續對話"
+        renderButton={testDataButton()}
+      />
+      <LoadingMask isFetching={isLoading} showSpinner={isLoading}>
+        {totalReviewingArticle === 0 && (
+          <EmptyBox type={EmptyBox.Type.ReviewingArticle} />
+        )}
+        {showingReviewingArticle.length > 0 && (
+          <>
+            <TabletAndAbove>
+              <ReviewingArticleList>
+                {showingReviewingArticle.map((article, idx) => {
+                  return (
+                    <div key={idx}>
+                      <ReviewingCard {...article} />
+                      <DividerContainer>
+                        <Divider />
+                      </DividerContainer>
+                    </div>
+                  )
+                })}
+                {showMore && (
+                  <MoreButton
+                    text="展開更多"
+                    rightIconComponent={<Arrow direction="down" />}
+                    onClick={handleShowMoreClick}
+                  />
+                )}
+              </ReviewingArticleList>
+            </TabletAndAbove>
+            <MobileOnly>
+              <ReviewingArticleList>
+                <Swiper
+                  slidesPerView={'auto'}
+                  spaceBetween={24}
+                  freeMode={true}
+                >
+                  {reviewingArticles.map((article, idx) => (
+                    <SwiperSlide key={idx}>
+                      <ReviewingCard {...article} />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </ReviewingArticleList>
+            </MobileOnly>
+          </>
+        )}
+      </LoadingMask>
     </ReviewingArticleContainer>
   )
 }
