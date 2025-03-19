1c6342489d9494d2af86b66ae7872c3e03b71c06
Aylie Chou <aylie_chou@twreporter.org>
2025-03-19 18:27:16 +0800

    fix: hard code promotions on member page

--- src/components/member-page/member-data.js
+++ src/components/member-page/member-data.js
@@ -4,13 +4,18 @@ import PropTypes from 'prop-types'
 
 import Divider from '@twreporter/react-components/lib/divider'
 import { H1, H3 } from '@twreporter/react-components/lib/text/headline'
-import { P1 } from '@twreporter/react-components/lib/text/paragraph'
+import { P1, P2 } from '@twreporter/react-components/lib/text/paragraph'
 import { colorGrayscale } from '@twreporter/core/lib/constants/color'
 import { MEMBER_ROLE } from '@twreporter/core/lib/constants/member-role'
 import {
   READING_TIME_UNIT,
   READING_TIME_UNIT_PAGE_TEXT,
 } from '@twreporter/core/lib/constants/reading-time-unit'
+// lodash
+import map from 'lodash/map'
+const _ = {
+  map,
+}
 
 const MemberDataContainer = styled.div`
   color: ${colorGrayscale.gray800};
@@ -48,7 +53,92 @@ const BottomDividerContainer = styled.div`
   margin-top: 24px;
 `
 
-const OnlyForGTM = styled.div``
+const Promotion = styled.div`
+  ${props => (props.$hide ? 'display: none;' : '')}
+  margin-top: 24px;
+  & {
+    color: ${colorGrayscale.gray600};
+  }
+  a,
+  a:active,
+  a:visited {
+    color: ${colorGrayscale.gray600};
+  }
+  a {
+    text-underline-offset: 4px;
+    text-decoration-line: underline !important;
+  }
+`
+
+const promotion = {
+  [MEMBER_ROLE.trailblazer]: [
+    <P2 key="promotion-trailblazer-1">
+      ・申請成為國家兩廳院「廳院人」會員（
+      <a
+        target="_blank"
+        href="https://docs.google.com/forms/d/108kjczL7_zva7p0sQMWoHFPqWGIfvMavgik5HYX8mM4/edit"
+        rel="noopener noreferrer"
+      >
+        會籍申請／展延
+      </a>
+      ｜
+      <a
+        target="_blank"
+        href="https://npac-ntch.org/members"
+        rel="noopener noreferrer"
+      >
+        查詢會籍
+      </a>
+      ）
+    </P2>,
+    <P2 key="promotion-trailblazer-2">
+      ・報導者出版品與周邊 85 折（請至
+      <a
+        target="_blank"
+        href="https://twreporter.waca.ec/"
+        rel="noopener noreferrer"
+      >
+        報導者 Books & Goods
+      </a>
+      使用折扣碼：KU8R6aY9Mx36）
+    </P2>,
+    <P2 key="promotion-trailblazer-3">
+      ・讀墨 Readmoo 報導者電子書 85 折（請至
+      <a
+        target="_blank"
+        href="https://readmoo.pse.is/79gkpv"
+        rel="noopener noreferrer"
+      >
+        讀墨平台
+      </a>
+      使用折扣碼：thereporter-15off）
+    </P2>,
+  ],
+  [MEMBER_ROLE.action_taker]: [
+    <P2 key="promotion-action-taker-1">
+      ・報導者出版品與周邊 9 折（請至
+      <a
+        target="_blank"
+        href="https://twreporter.waca.ec/"
+        rel="noopener noreferrer"
+      >
+        報導者 Books & Goods
+      </a>
+      使用折扣碼：LLsWj66BFr3v）
+    </P2>,
+    <P2 key="promotion-action-taker-2">
+      ・讀墨 Readmoo 報導者電子書 9 折（請至
+      <a
+        target="_blank"
+        href="https://readmoo.pse.is/79gkpv"
+        rel="noopener noreferrer"
+      >
+        讀墨平台
+      </a>
+      使用折扣碼：thereporter-10off）
+    </P2>,
+  ],
+}
 
 const MemberData = ({
   role = { key: MEMBER_ROLE.explorer, name: '' },
@@ -126,7 +216,10 @@ const MemberData = ({
       <BottomDividerContainer>
         <Divider />
       </BottomDividerContainer>
-      <OnlyForGTM name="merchandise-promo-code" />
+      <Promotion $hide={role.key === MEMBER_ROLE.explorer}>
+        <P2 text={`${role.name}回饋`} weight={P2.Weight.BOLD} />
+        {_.map(promotion[role.key], jsx => jsx)}
+      </Promotion>
     </MemberDataContainer>
   )
 }
